6440e0c28d2fbd8267d271e01b827a1609414cf2
tsai <jackalcooper@gmail.com>
2022-06-05 14:51:03 +0800

    Move transformation into mlir/1

--- apps/beaver_mlir/lib/beaver/mlir.ex
+++ apps/beaver_mlir/lib/beaver/mlir.ex
@@ -40,11 +40,65 @@ defmodule Beaver.MLIR do
     block_ast
   end
 
+  # TODO: check sigil_t is from MLIR
   defmacro region(do: block) do
+    new_block_ast =
+      Macro.prewalk(block, fn
+        # one SSA
+        {:"::", _,
+         [
+           {:=, line2,
+            [
+              var,
+              {mf, line3, args_ast}
+            ]},
+           sigil_t = {:sigil_t, _, _}
+         ]} ->
+          arguments_ast =
+            quote do
+              [unquote_splicing(args_ast), result_types: unquote(sigil_t)]
+            end
+
+          # TODO, check if last arg is keyword
+          {:=, line2,
+           [
+             var,
+             {mf, line3, [arguments_ast]}
+           ]}
+
+        #  block arg
+        {:"::", _,
+         [
+           var = {_var_name, _, nil},
+           sigil_t = {:sigil_t, _, _}
+         ]} ->
+          quote do
+            {unquote(var), unquote(sigil_t)}
+          end
+
+        #  expression with no binding
+        {:"::", _,
+         [
+           {mf, line2, args_ast},
+           sigil_t = {:sigil_t, _, _}
+         ]} ->
+          arguments_ast =
+            quote do
+              [unquote_splicing(args_ast), result_types: unquote(sigil_t)]
+            end
+
+          {mf, line2, [arguments_ast]}
+
+        other ->
+          other
+      end)
+
+    new_block_ast |> Macro.to_string() |> IO.puts()
+
     quote do
       func_region = Beaver.MLIR.CAPI.mlirRegionCreate()
       Beaver.MLIR.Managed.Region.set(func_region)
-      unquote(block)
+      unquote(new_block_ast)
       Beaver.MLIR.Managed.Terminator.resolve()
     end
   end
--- apps/beaver_mlir/lib/beaver/mlir/dialect/builtin.ex
+++ apps/beaver_mlir/lib/beaver/mlir/dialect/builtin.ex
@@ -5,60 +5,6 @@ defmodule Beaver.MLIR.Dialect.Builtin do
   end
 
   defmacro module(do: block) do
-    # TODO: check sigil_t is from MLIR
-    new_block_ast =
-      Macro.prewalk(block, fn
-        # one SSA
-        {:"::", _,
-         [
-           {:=, line2,
-            [
-              var,
-              {mf, line3, args_ast}
-            ]},
-           sigil_t = {:sigil_t, _, _}
-         ]} ->
-          arguments_ast =
-            quote do
-              [unquote_splicing(args_ast), result_types: unquote(sigil_t)]
-            end
-
-          # TODO, check if last arg is keyword
-          {:=, line2,
-           [
-             var,
-             {mf, line3, [arguments_ast]}
-           ]}
-
-        #  block arg
-        {:"::", _,
-         [
-           var = {_var_name, _, nil},
-           sigil_t = {:sigil_t, _, _}
-         ]} ->
-          quote do
-            {unquote(var), unquote(sigil_t)}
-          end
-
-        #  expression with no binding
-        {:"::", _,
-         [
-           {mf, line2, args_ast},
-           sigil_t = {:sigil_t, _, _}
-         ]} ->
-          arguments_ast =
-            quote do
-              [unquote_splicing(args_ast), result_types: unquote(sigil_t)]
-            end
-
-          {mf, line2, [arguments_ast]}
-
-        other ->
-          other
-      end)
-
-    new_block_ast |> Macro.to_string() |> IO.puts()
-
     quote do
       location = Beaver.MLIR.Managed.Location.get()
       module = Beaver.MLIR.CAPI.mlirModuleCreateEmpty(location)
@@ -69,7 +15,7 @@ defmodule Beaver.MLIR.Dialect.Builtin do
       end
 
       Beaver.MLIR.Managed.InsertionPoint.push(__module__insert_point__)
-      unquote(new_block_ast)
+      unquote(block)
       Beaver.MLIR.Managed.InsertionPoint.pop()
 
       Beaver.MLIR.Managed.Block.clear_ids()
