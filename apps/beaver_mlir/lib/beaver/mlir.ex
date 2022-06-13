203bf8a3588baa6fec68d064a086e45cc1dade57
Shenghang Tsai <jackalcooper@gmail.com>
2022-06-14 07:13:06 +0800

    Use value in previous block (#9)
    * Remove Block.under
    
    * Fix op appending in module
    
    * Refine wording

--- apps/beaver_mlir/lib/beaver/mlir.ex
+++ apps/beaver_mlir/lib/beaver/mlir.ex
@@ -20,14 +20,18 @@ defmodule Beaver.MLIR do
 
         block = Beaver.MLIR.Block.create(block_arg_types, block_arg_locs)
 
-        Beaver.MLIR.Block.under(block, fn ->
-          unquote_splicing(block_arg_var_ast)
-          unquote(block)
-        end)
+        # can't put code here inside a function like Region.under, because we need to support uses across blocks
+        previous_block = Beaver.MLIR.Managed.Block.get()
+        Beaver.MLIR.Managed.Block.set(block)
+        unquote_splicing(block_arg_var_ast)
+        unquote(block)
+        Beaver.MLIR.Managed.Block.set(previous_block)
 
         if region = Beaver.MLIR.Managed.Region.get() do
           Beaver.MLIR.CAPI.mlirRegionAppendOwnedBlock(region, block)
           Beaver.MLIR.Managed.Terminator.put_block(unquote(block_id), block)
+        else
+          raise "no managed region found"
         end
       end
 
--- apps/beaver_mlir/lib/beaver/mlir/block.ex
+++ apps/beaver_mlir/lib/beaver/mlir/block.ex
@@ -42,6 +42,9 @@ defmodule Beaver.MLIR.Block do
     )
   end
 
+  @doc """
+  run function f and append ops created to block. This function only works when there is no uses across blocks. For instance, a ModuleOp has only one region of one block.
+  """
   def under(block, f) when is_function(f, 0) do
     previous_block = Beaver.MLIR.Managed.Block.get()
     Beaver.MLIR.Managed.Block.set(block)
