1cded1fb4d527a6a9cab2d190cb70665a076881e
tsai <jackalcooper@gmail.com>
2022-06-13 21:58:26 +0800

    Move region management out of macro

--- apps/beaver_mlir/lib/beaver/mlir.ex
+++ apps/beaver_mlir/lib/beaver/mlir.ex
@@ -98,9 +98,7 @@ defmodule Beaver.MLIR do
 
     new_block_ast =
       quote do
-        outer_region = Beaver.MLIR.Managed.Region.get()
         region = Beaver.MLIR.CAPI.mlirRegionCreate()
-        Beaver.MLIR.Managed.Region.set(region)
 
         Beaver.MLIR.Region.create_blocks(region, fn ->
           var!(beaver_blocks_to_be_append, Beaver.MLIR) = []
@@ -108,7 +106,6 @@ defmodule Beaver.MLIR do
           var!(beaver_blocks_to_be_append, Beaver.MLIR)
         end)
 
-        Beaver.MLIR.Managed.Region.set(outer_region)
         [region]
       end
 
--- apps/beaver_mlir/lib/beaver/mlir/region.ex
+++ apps/beaver_mlir/lib/beaver/mlir/region.ex
@@ -1,6 +1,10 @@
 defmodule Beaver.MLIR.Region do
   def create_blocks(region, f) when is_function(f, 0) do
+    outer_region = Beaver.MLIR.Managed.Region.get()
+    Beaver.MLIR.Managed.Region.set(region)
     blocks = f.()
+    Beaver.MLIR.Managed.Region.set(outer_region)
+
     if not is_list(blocks), do: raise("Expected a list of blocks")
 
     for b <- blocks do
