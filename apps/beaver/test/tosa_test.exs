826aa1304c97d97ea1d7b458147037af15e0da0e
tsai <jackalcooper@gmail.com>
2022-06-14 23:21:19 +0800

    add test case for creating tosa

---
+++ apps/beaver/test/tosa_test.exs
@@ -0,0 +1,32 @@
+defmodule TosaTest do
+  use ExUnit.Case
+  alias Beaver.MLIR
+  import Beaver.MLIR.Sigils
+
+  test "generate and run tosa" do
+    require Beaver
+    require Beaver.MLIR.Dialect.Func
+    alias Beaver.MLIR
+    alias Beaver.MLIR.Dialect.{Builtin, Func, Arith, CF, Tosa}
+    import Builtin, only: :macros
+    import MLIR, only: :macros
+    import MLIR.Sigils
+
+    Beaver.mlir do
+      Builtin.module do
+        Func.func test_multibroadcast(
+                    function_type: ~a"(tensor<1x3xf32>, tensor<2x1xf32>) -> tensor<2x3xf32>"
+                  ) do
+          region do
+            block entry(arg0 :: ~t{tensor<1x3xf32>}, arg1 :: ~t{tensor<2x1xf32>}) do
+              v0 = Tosa.add(arg0, arg1) :: ~t{tensor<2x3xf32>}
+              Func.return(v0)
+            end
+          end
+        end
+      end
+    end
+    |> MLIR.Operation.dump!()
+    |> MLIR.Operation.verify!()
+  end
+end
