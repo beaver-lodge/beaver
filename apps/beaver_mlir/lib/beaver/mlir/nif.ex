f5ac91fb7ce2b9e415eb2fe4c5ebfae7f30e91e1
tsai <jackalcooper@gmail.com>
2022-06-15 21:48:37 +0800

    add ext src for Beaver.MLIR.NIF

--- apps/beaver_mlir/lib/beaver/mlir/nif.ex
+++ apps/beaver_mlir/lib/beaver/mlir/nif.ex
@@ -1,4 +1,12 @@
 defmodule Beaver.MLIR.NIF do
+  for path <- Path.wildcard("native/mlir_nif/met/include/**/*.h") do
+    @external_resource path
+  end
+
+  for path <- Path.wildcard(Path.join(Beaver.LLVM.Config.include_dir(), "**/*.h")) do
+    @external_resource path
+  end
+
   # This will persitent the nif library path so we can later open it
   Module.register_attribute(__MODULE__, :load_from, accumulate: false, persist: true)
   use Rustler, otp_app: :beaver_mlir, crate: "mlir_nif"
