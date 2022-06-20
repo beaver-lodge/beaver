814a2fbe8d7f55dfcc83771306a4c2280072636c
tsai <jackalcooper@gmail.com>
2022-06-20 19:50:05 +0800

    add tests for attr parsing

--- apps/beaver_mlir/lib/beaver/mlir/attribute.ex
+++ apps/beaver_mlir/lib/beaver/mlir/attribute.ex
@@ -2,10 +2,22 @@ defmodule Beaver.MLIR.Attribute do
   alias Beaver.MLIR.CAPI
   alias Beaver.MLIR
 
-  def get(attr) when is_binary(attr) do
+  def is_null(jit) do
+    jit
+    |> Exotic.Value.fetch(MLIR.CAPI.MlirAttribute, :ptr)
+    |> Exotic.Value.extract() == 0
+  end
+
+  def get(attr_str) when is_binary(attr_str) do
     ctx = MLIR.Managed.Context.get()
-    attr = MLIR.StringRef.create(attr)
-    CAPI.mlirAttributeParseGet(ctx, attr)
+    attr = MLIR.StringRef.create(attr_str)
+    attr = CAPI.mlirAttributeParseGet(ctx, attr)
+
+    if is_null(attr) do
+      raise "fail to parse attribute: #{attr_str}"
+    end
+
+    attr
   end
 
   def unit() do
