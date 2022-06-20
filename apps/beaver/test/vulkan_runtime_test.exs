186ce69651ed8d69b817769504546dd37c29ba2c
tsai <jackalcooper@gmail.com>
2022-06-20 08:40:33 +0800

    add example running passes to lower to spirv

---
+++ apps/beaver/test/vulkan_runtime_test.exs
@@ -0,0 +1,90 @@
+defmodule VulkanRuntimeTest do
+  use ExUnit.Case
+  alias Beaver.MLIR
+
+  test "run ir with vulkan" do
+    import Beaver.MLIR.Sigils
+    import MLIR.{Transforms, Conversion}
+
+    arg = Exotic.Value.get(42)
+    return = Exotic.Value.get(-1)
+
+    jit =
+      ~m"""
+      module attributes {
+        gpu.container_module,
+        spv.target_env = #spv.target_env<
+          #spv.vce<v1.0, [Shader], [SPV_KHR_storage_buffer_storage_class]>, #spv.resource_limits<>>
+      } {
+        gpu.module @kernels {
+          gpu.func @kernel_addi(%arg0 : memref<8xi32>, %arg1 : memref<8x8xi32>, %arg2 : memref<8x8x8xi32>)
+            kernel attributes { spv.entry_point_abi = #spv.entry_point_abi<local_size = dense<[1, 1, 1]>: vector<3xi32>>} {
+            %x = gpu.block_id x
+            %y = gpu.block_id y
+            %z = gpu.block_id z
+            %0 = memref.load %arg0[%x] : memref<8xi32>
+            %1 = memref.load %arg1[%y, %x] : memref<8x8xi32>
+            %2 = arith.addi %0, %1 : i32
+            memref.store %2, %arg2[%z, %y, %x] : memref<8x8x8xi32>
+            gpu.return
+          }
+        }
+
+        func.func @main() {
+          %arg0 = memref.alloc() : memref<8xi32>
+          %arg1 = memref.alloc() : memref<8x8xi32>
+          %arg2 = memref.alloc() : memref<8x8x8xi32>
+          %value0 = arith.constant 0 : i32
+          %value1 = arith.constant 1 : i32
+          %value2 = arith.constant 2 : i32
+          %arg3 = memref.cast %arg0 : memref<8xi32> to memref<?xi32>
+          %arg4 = memref.cast %arg1 : memref<8x8xi32> to memref<?x?xi32>
+          %arg5 = memref.cast %arg2 : memref<8x8x8xi32> to memref<?x?x?xi32>
+          call @fillResource1DInt(%arg3, %value1) : (memref<?xi32>, i32) -> ()
+          call @fillResource2DInt(%arg4, %value2) : (memref<?x?xi32>, i32) -> ()
+          call @fillResource3DInt(%arg5, %value0) : (memref<?x?x?xi32>, i32) -> ()
+
+          %cst1 = arith.constant 1 : index
+          %cst8 = arith.constant 8 : index
+          gpu.launch_func @kernels::@kernel_addi
+              blocks in (%cst8, %cst8, %cst8) threads in (%cst1, %cst1, %cst1)
+              args(%arg0 : memref<8xi32>, %arg1 : memref<8x8xi32>, %arg2 : memref<8x8x8xi32>)
+          %arg6 = memref.cast %arg5 : memref<?x?x?xi32> to memref<*xi32>
+          call @printMemrefI32(%arg6) : (memref<*xi32>) -> ()
+          return
+        }
+        func.func private @fillResource1DInt(%0 : memref<?xi32>, %1 : i32)
+        func.func private @fillResource2DInt(%0 : memref<?x?xi32>, %1 : i32)
+        func.func private @fillResource3DInt(%0 : memref<?x?x?xi32>, %1 : i32)
+        func.func private @printMemrefI32(%ptr : memref<*xi32>)
+      }
+      """
+      |> canonicalize
+      |> cse
+      |> gpu_kernel_outlining
+      |> MLIR.Pass.Composer.pipeline("fold-memref-subview-ops")
+      |> convert_gpu_to_spirv
+      |> MLIR.Pass.Composer.nested("spv.module", fn pm ->
+        MLIR.Pass.pipeline!(pm, "spirv-lower-abi-attrs")
+        MLIR.Pass.pipeline!(pm, "spirv-update-vce")
+      end)
+      |> convert_gpu_launch_to_vulkan_launch
+      |> convert_memref_to_llvm
+      |> MLIR.Pass.Composer.nested("func.func", fn pm ->
+        MLIR.Pass.pipeline!(pm, "llvm-request-c-wrappers")
+      end)
+      |> convert_func_to_llvm
+      |> reconcile_unrealized_casts
+      |> launch_func_to_vulkan
+      |> MLIR.Pass.Composer.run!()
+      |> MLIR.Operation.dump!()
+      |> MLIR.ExecutionEngine.create!()
+
+    for i <- 0..100_0 do
+      Task.async(fn ->
+        MLIR.ExecutionEngine.invoke!(jit, "main", [])
+      end)
+    end
+    |> Task.await_many()
+  end
+end
