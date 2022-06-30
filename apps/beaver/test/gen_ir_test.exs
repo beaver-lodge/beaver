40ac513230c85c49b4ef0210aa69c0d091e5afb6
tsai <jackalcooper@gmail.com>
2022-06-30 22:55:04 +0800

    update types in GenIRTest

--- apps/beaver/test/gen_ir_test.exs
+++ apps/beaver/test/gen_ir_test.exs
@@ -8,6 +8,7 @@ defmodule GenIRTest do
     import Beaver
     require Beaver.MLIR.Dialect.Func
     alias Beaver.MLIR
+    alias MLIR.{Type, Attribute}
     alias Beaver.MLIR.Dialect.{Builtin, Func, Arith, CF}
     import Builtin, only: :macros
     import MLIR, only: :macros
@@ -18,20 +19,20 @@ defmodule GenIRTest do
         Func.func some_func(function_type: ~a"() -> i32") do
           region do
             block bb_entry() do
-              v0 = Arith.constant({:value, ~a{0: i32}}) :: ~t<i32>
+              v0 = Arith.constant({:value, Attribute.integer(Type.i(32), 0)}) :: Type.i(32)
               cond0 = Arith.constant(true)
               CF.cond_br(cond0, :bb1, {:bb2, [v0]})
             end
 
             block bb1() do
-              v1 = Arith.constant({:value, ~a{0: i32}}) :: ~t<i32>
-              _add = Arith.addi(v0, v0) :: ~t<i32>
+              v1 = Arith.constant({:value, Attribute.integer(Type.i(32), 0)}) :: Type.i(32)
+              _add = Arith.addi(v0, v0) :: Type.i(32)
               CF.br({:bb2, [v1]})
             end
 
-            block bb2(arg :: ~t<i32>) do
-              v2 = Arith.constant({:value, ~a{0: i32}}) :: ~t<i32>
-              add = Arith.addi(arg, v2) :: ~t<i32>
+            block bb2(arg :: Type.i(32)) do
+              v2 = Arith.constant({:value, Attribute.integer(Type.i(32), 0)}) :: Type.i(32)
+              add = Arith.addi(arg, v2) :: Type.i(32)
               Func.return(add)
             end
           end
@@ -40,17 +41,17 @@ defmodule GenIRTest do
         Func.func some_func2(function_type: ~a"() -> i32") do
           region do
             block bb_entry() do
-              v0 = Arith.constant({:value, ~a{0: i32}}) :: ~t<i32>
-              _add = Arith.addi(v0, v0) :: ~t<i32>
+              v0 = Arith.constant({:value, Attribute.integer(Type.i(32), 0)}) :: Type.i(32)
+              _add = Arith.addi(v0, v0) :: Type.i(32)
               CF.br({:bb1, [v0]})
             end
 
-            block bb1(arg :: ~t<i32>) do
-              v2 = Arith.constant({:value, ~a{0: i32}}) :: ~t<i32>
-              add = Arith.addi(arg, v2) :: ~t<i32>
-              _sub = Arith.subi(arg, v2) :: ~t<i32>
-              _mul = Arith.muli(arg, v2) :: ~t<i32>
-              _div = Arith.divsi(arg, v2) :: ~t<i32>
+            block bb1(arg :: Type.i(32)) do
+              v2 = Arith.constant({:value, Attribute.integer(Type.i(32), 0)}) :: Type.i(32)
+              add = Arith.addi(arg, v2) :: Type.i(32)
+              _sub = Arith.subi(arg, v2) :: Type.i(32)
+              _mul = Arith.muli(arg, v2) :: Type.i(32)
+              _div = Arith.divsi(arg, v2) :: Type.i(32)
               Func.return(add)
             end
           end
