0c6f3a628374f07a75d98369d36b44c965edd8d9
Shenghang Tsai <jackalcooper@gmail.com>
2022-07-03 15:32:57 +0800

    Support matching attributes (#30)
    * add note on prototype
    
    * basic support for matching attributes
    
    * fix typo in docs
    
    * refine docs
    
    * rm todo
    
    * add more docs

--- apps/beaver/lib/beaver/dsl/pattern.ex
+++ apps/beaver/lib/beaver/dsl/pattern.ex
@@ -14,16 +14,26 @@ defmodule Beaver.DSL.Pattern do
        ) do
     operands = map_args |> Keyword.get(:operands, [])
     results = map_args |> Keyword.get(:results, [])
-    _attributes = map_args |> Keyword.get(:attributes, [])
+    attributes = map_args |> Keyword.get(:attributes, [])
 
+    # Transform unbound variables of operands. If is bound, it means it is another operation's result (get bound in a previous expression)
     filtered_operands =
       Enum.map(operands, fn
         {:bound, bound} -> bound
         other -> other
       end)
 
-    map_args = Keyword.put(map_args, :operands, filtered_operands)
+    # extract unbound variables from attributes keyword
+    filtered_attributes =
+      Enum.map(attributes, fn
+        {_k, {:bound, bound}} -> bound
+        {_k, other} -> other
+      end)
 
+    map_args = Keyword.put(map_args, :operands, filtered_operands)
+    map_args = Keyword.put(map_args, :attributes, filtered_attributes)
+
+    # generate bounds between operand variables and pdl.operand
     operands =
       for operand <- operands do
         case operand do
@@ -39,6 +49,35 @@ defmodule Beaver.DSL.Pattern do
         end
       end
 
+    # generate bounds between attribute variables and pdl.attribute
+    attributes_match =
+      for {_name, attribute} <- attributes do
+        case attribute do
+          {:bound, bound} ->
+            quote do
+              unquote(bound) =
+                case unquote(bound) do
+                  # if bound variable is a pdl.attribute's result value
+                  %Beaver.MLIR.CAPI.MlirValue{} ->
+                    unquote(bound)
+
+                  # if bound variable is a real attribute
+                  %Beaver.MLIR.CAPI.MlirAttribute{} ->
+                    Beaver.MLIR.Dialect.PDL.attribute(value: unquote(bound)) >>>
+                      ~t{!pdl.attribute}
+                end
+            end
+
+          _ ->
+            quote do
+              unquote(attribute) = Beaver.MLIR.Dialect.PDL.attribute() >>> ~t{!pdl.attribute}
+            end
+        end
+      end
+
+    attributes_keys = Keyword.keys(attributes)
+
+    # generate bounds between result variables and pdl.type
     results_match =
       for result <- results do
         quote do
@@ -46,6 +85,8 @@ defmodule Beaver.DSL.Pattern do
         end
       end
 
+    # generate bounds between result variables and pdl.value produced by pdl.result
+    # preceding expression should use pin operator (^var) to access these version of binding otherwise it will get bound to a pdl.value produced by pdl.operand
     results_rebind =
       for {result, i} <- Enum.with_index(results) do
         quote do
@@ -57,16 +98,27 @@ defmodule Beaver.DSL.Pattern do
         end
       end
 
+    # injecting all variables and dispatch the op creation
     ast =
       quote do
         unquote_splicing(operands)
+        unquote_splicing(attributes_match)
         unquote_splicing(results_match)
+
+        attribute_names =
+          for key <- unquote(attributes_keys) do
+            key
+            |> Atom.to_string()
+            |> Beaver.MLIR.Attribute.string()
+          end
+          |> Beaver.MLIR.Attribute.array()
 
         beaver_gen_root =
           Beaver.DSL.Op.Prototype.dispatch(
             unquote(struct_name),
             unquote(map_args),
-            &Beaver.DSL.Pattern.create_root/2
+            attribute_names,
+            &Beaver.DSL.Pattern.create_root/3
           )
 
         unquote_splicing(results_rebind)
@@ -106,7 +158,8 @@ defmodule Beaver.DSL.Pattern do
 
   def create_root(
         op_name,
-        %Beaver.DSL.Op.Prototype{operands: operands, attributes: attributes, results: results}
+        %Beaver.DSL.Op.Prototype{operands: operands, attributes: attributes, results: results},
+        %Beaver.MLIR.CAPI.MlirAttribute{} = attribute_names
       ) do
     mlir do
       arguments = operands ++ attributes ++ results
@@ -115,7 +168,7 @@ defmodule Beaver.DSL.Pattern do
         arguments ++
           [
             name: Beaver.MLIR.Attribute.string(op_name),
-            attributeNames: Beaver.MLIR.Attribute.array([]),
+            attributeNames: attribute_names,
             operand_segment_sizes:
               Beaver.MLIR.ODS.operand_segment_sizes([
                 length(operands),
