67c205f26e07191d3758cd33899e76475fd3dffb
tsai <jackalcooper@gmail.com>
2022-07-04 21:31:06 +0800

    add handle_invoke in using

--- apps/beaver/lib/beaver/mlir/pass.ex
+++ apps/beaver/lib/beaver/mlir/pass.ex
@@ -2,13 +2,29 @@ defmodule Beaver.MLIR.Pass do
   use Beaver
   alias MLIR.CAPI
 
+  @callback run(MLIR.CAPI.MlirOperation.t()) :: :ok | :error
+
   defmacro __using__(opts) do
     quote(bind_quoted: [opts: opts]) do
       @behaviour MLIR.Pass
+
+      def handle_invoke(
+            :run = id,
+            [
+              %Beaver.MLIR.CAPI.MlirOperation{} = op,
+              %Beaver.MLIR.CAPI.MlirExternalPass{} = pass,
+              userData
+            ],
+            state
+          ) do
+        with :ok <- run() do
+          {:return, userData, state}
+        else
+          :error -> :error
+        end
+      end
     end
   end
-
-  @callback run(MLIR.CAPI.MlirOperation.t()) :: :ok | :error
 
   def pipeline!(pm, pipeline_str) when is_binary(pipeline_str) do
     status = CAPI.mlirParsePassPipeline(pm, MLIR.StringRef.create(pipeline_str))
