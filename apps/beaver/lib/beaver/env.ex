814efd4c475632110d1c36810e6330a530f2aaed
tsai <jackalcooper@gmail.com>
2022-07-05 22:36:51 +0800

    add set_container/1 and cf test

--- apps/beaver/lib/beaver/env.ex
+++ apps/beaver/lib/beaver/env.ex
@@ -1,4 +1,6 @@
 defmodule Beaver.Env do
+  alias Beaver.MLIR
+
   @moduledoc """
   Provide macros to insert MLIR context and IR element of structure. These macros are designed to mimic the behavior and aesthetics of __MODULE__/0, __CALLER__/0 in Elixir.
   Its distinguished form is to indicate this should not be expected to be a function or a macro works like a function.
@@ -36,5 +38,14 @@ defmodule Beaver.Env do
 
   defmacro mlir__LOCATION__() do
     Beaver.MLIR.Managed.Location.get()
+  end
+
+  @doc """
+  Setting a container. It could be a module/parent/region/block.
+  When a higher-level container is set, all lower-level containers already set will be cleared.
+  When a lower-level container is set, it will be checked it is under the higher-level containers.
+  """
+  def set_container(%MLIR.CAPI.MlirBlock{} = block) do
+    MLIR.Managed.Block.set(block)
   end
 end
