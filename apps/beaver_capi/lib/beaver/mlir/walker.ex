fadecfec905b1cf379b76c488a9ec4ea7606f3f2
tsai <jackalcooper@gmail.com>
2022-07-06 21:20:09 +0800

    add another set of funcs

--- apps/beaver_capi/lib/beaver/mlir/walker.ex
+++ apps/beaver_capi/lib/beaver/mlir/walker.ex
@@ -27,15 +27,19 @@ defmodule Beaver.MLIR.Walker do
           container: container(),
           container_module: element_module(),
           element_module: element_module(),
-          get_num: (container() -> Exotic.Value.t() | integer()),
-          get_element: (container() -> element()),
-          element_equal: (element(), element() -> Exotic.Value.t() | integer())
+          get_num: (container() -> Exotic.Value.t() | integer()) | nil,
+          get_element: (container(), integer() | Exotic.Value.t() -> element()) | nil,
+          element_equal: (element(), element() -> Exotic.Value.t() | integer()) | nil,
+          get_first: (container() -> element()) | nil,
+          get_next: (element() -> element()) | nil,
+          get_parent: (element() -> container()) | nil
         }
 
-  keys = [:container, :container_module, :element_module, :get_num, :get_element, :element_equal]
-
-  @enforce_keys keys
-  defstruct keys
+  container_keys = [:container, :container_module, :element_module]
+  index_func_keys = [:get_num, :get_element, :element_equal]
+  iter_func_keys = [:get_first, :get_next, :get_parent]
+  @enforce_keys [:container, :container_module, :element_module]
+  defstruct container_keys ++ index_func_keys ++ iter_func_keys
 
   # operands, results, attributes of one operation
   defp verify_nesting!(MlirOperation, MlirValue), do: :ok
@@ -48,37 +52,32 @@ defmodule Beaver.MLIR.Walker do
   defp verify_nesting!(MlirRegion, MlirBlock), do: :ok
   # operations in a block
   defp verify_nesting!(MlirBlock, MlirOperation), do: :ok
+  # arguments of a block
+  defp verify_nesting!(MlirBlock, MlirValue), do: :ok
 
   defp verify_nesting!(container_module, element_module) do
-    raise "not a legal structure could be walked in MLIR: #{inspect(container_module)} - #{inspect(element_module)}"
+    raise "not a legal structure could be walked in MLIR: #{inspect(container_module)}(#{inspect(element_module)})"
+  end
+
+  defp to_container(module = %MlirModule{}) do
+    CAPI.mlirModuleGetOperation(module)
+  end
+
+  defp to_container(container) do
+    container
   end
 
   def new(
-        container = %MlirModule{},
+        container,
         element_module,
-        get_num,
-        get_element,
-        element_equal
-      ) do
-    new(
-      CAPI.mlirModuleGetOperation(container),
-      element_module,
-      get_num,
-      get_element,
-      element_equal
-    )
-  end
-
-  def new(
-        container = %container_module{},
-        element_module,
-        get_num,
-        get_element,
-        element_equal
+        get_num: get_num,
+        get_element: get_element,
+        element_equal: element_equal
       )
       when is_function(get_num, 1) and
              is_function(get_element, 2) and
              is_function(element_equal, 2) do
+    container = %container_module{} = to_container(container)
     verify_nesting!(container_module, element_module)
 
     %__MODULE__{
@@ -91,14 +90,37 @@ defmodule Beaver.MLIR.Walker do
     }
   end
 
+  def new(
+        container,
+        element_module,
+        get_first: get_first,
+        get_next: get_next,
+        get_parent: get_parent
+      )
+      when is_function(get_first, 1) and
+             is_function(get_next, 1) and
+             is_function(get_parent, 1) do
+    container = %container_module{} = to_container(container)
+    verify_nesting!(container_module, element_module)
+
+    %__MODULE__{
+      container: container,
+      container_module: container_module,
+      element_module: element_module,
+      get_first: get_first,
+      get_next: get_next,
+      get_parent: get_parent
+    }
+  end
+
   @spec operands(MlirOperation.t()) :: Enumerable.result()
   def operands(op) do
     new(
       op,
       MlirValue,
-      &CAPI.mlirOperationGetNumOperands/1,
-      &CAPI.mlirOperationGetOperand/2,
-      &CAPI.mlirValueEqual/2
+      get_num: &CAPI.mlirOperationGetNumOperands/1,
+      get_element: &CAPI.mlirOperationGetOperand/2,
+      element_equal: &CAPI.mlirValueEqual/2
     )
   end
 
@@ -107,9 +129,9 @@ defmodule Beaver.MLIR.Walker do
     new(
       op,
       MlirValue,
-      &CAPI.mlirOperationGetNumResults/1,
-      &CAPI.mlirOperationGetResult/2,
-      &CAPI.mlirValueEqual/2
+      get_num: &CAPI.mlirOperationGetNumResults/1,
+      get_element: &CAPI.mlirOperationGetResult/2,
+      element_equal: &CAPI.mlirValueEqual/2
     )
   end
 
@@ -118,9 +140,9 @@ defmodule Beaver.MLIR.Walker do
     new(
       op,
       MlirValue,
-      &CAPI.mlirOperationGetNumRegions/1,
-      &CAPI.mlirOperationGetRegion/2,
-      &CAPI.mlirRegionEqual/2
+      get_num: &CAPI.mlirOperationGetNumRegions/1,
+      get_element: &CAPI.mlirOperationGetRegion/2,
+      element_equal: &CAPI.mlirRegionEqual/2
     )
   end
 
@@ -129,9 +151,9 @@ defmodule Beaver.MLIR.Walker do
     new(
       op,
       MlirBlock,
-      &CAPI.mlirOperationGetNumSuccessors/1,
-      &CAPI.mlirOperationGetSuccessor/2,
-      &CAPI.mlirBlockEqual/2
+      get_num: &CAPI.mlirOperationGetNumSuccessors/1,
+      get_element: &CAPI.mlirOperationGetSuccessor/2,
+      element_equal: &CAPI.mlirBlockEqual/2
     )
   end
 
@@ -140,21 +162,34 @@ defmodule Beaver.MLIR.Walker do
     new(
       op,
       MlirAttribute,
-      &CAPI.mlirOperationGetNumAttributes/1,
-      &CAPI.mlirOperationGetAttribute/2,
-      &CAPI.mlirAttributeEqual/2
+      get_num: &CAPI.mlirOperationGetNumAttributes/1,
+      get_element: &CAPI.mlirOperationGetAttribute/2,
+      element_equal: &CAPI.mlirAttributeEqual/2
     )
   end
 
-  # def blocks(region) do
-  #   new(
-  #     region,
-  #     MlirOperation,
-  #     &CAPI.mlirOperationGetNumSuccessors/1,
-  #     &CAPI.mlirOperationGetSuccessor/2,
-  #     &CAPI.mlirBlockEqual/2
-  #   )
-  # end
+  def arguments(block) do
+    new(
+      block,
+      MlirValue,
+      get_num: &CAPI.mlirBlockGetNumArguments/1,
+      get_element: &CAPI.mlirBlockGetArgument/2,
+      element_equal: &CAPI.mlirValueEqual/2
+    )
+  end
+
+  def operations(block) do
+  end
+
+  def blocks(region) do
+    new(
+      region,
+      MlirBlock,
+      get_first: &CAPI.mlirRegionGetFirstBlock/1,
+      get_next: &CAPI.mlirBlockGetNextInRegion/1,
+      get_parent: &CAPI.mlirBlockGetParentRegion/1
+    )
+  end
 end
 
 alias Beaver.MLIR.Walker
@@ -177,7 +212,8 @@ defimpl Enumerable, for: Walker do
 
   @spec slice(Walker.t()) ::
           {:ok, size :: non_neg_integer(), Enumerable.slicing_fun()} | {:error, module()}
-  def slice(walker = %Walker{container: container, get_element: get_element}) do
+  def slice(walker = %Walker{container: container, get_element: get_element})
+      when is_function(get_element, 2) do
     {:ok, count(walker),
      fn start, length ->
        pos_range = start..(start + length - 1)
@@ -197,7 +233,8 @@ defimpl Enumerable, for: Walker do
     do: {:suspended, acc, &reduce(walker, &1, fun)}
 
   # Reduce all in one :cont
-  def reduce(%Walker{container: container, get_element: get_element} = walker, {:cont, acc}, fun) do
+  def reduce(%Walker{container: container, get_element: get_element} = walker, {:cont, acc}, fun)
+      when is_function(get_element, 2) do
     pos_range = 0..(count(walker) - 1)//1
 
     Enum.reduce(pos_range, acc, fn pos, acc ->
