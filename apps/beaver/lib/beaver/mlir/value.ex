660e8becfdc8baad7295bd9fc46455863c550460
tsai <jackalcooper@gmail.com>
2022-07-11 21:10:13 +0800

    add docs

--- apps/beaver/lib/beaver/mlir/value.ex
+++ apps/beaver/lib/beaver/mlir/value.ex
@@ -9,10 +9,16 @@ defmodule Beaver.MLIR.Value do
     CAPI.mlirValueIsABlockArgument(value) |> Exotic.Value.extract()
   end
 
+  @doc """
+  Returns true if the value is a result of an operation.
+  """
   def result?(%CAPI.MlirValue{} = value) do
     CAPI.mlirValueIsAOpResult(value) |> Exotic.Value.extract()
   end
 
+  @doc """
+  Return the defining op of this value if this value is a result
+  """
   def owner(value) do
     if result?(value) do
       {:ok, CAPI.mlirOpResultGetOwner(value)}
