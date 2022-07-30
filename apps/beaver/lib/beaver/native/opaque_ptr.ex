8e2f3be15a889a3ab2b2e965de7b6ad268be2d4a
tsai <jackalcooper@gmail.com>
2022-07-30 13:33:23 +0800

    rm dumping

--- apps/beaver/lib/beaver/native/opaque_ptr.ex
+++ apps/beaver/lib/beaver/native/opaque_ptr.ex
@@ -16,7 +16,6 @@ defmodule Beaver.Native.OpaquePtr do
   """
   def to_resource(mod, %Beaver.Native.OpaquePtr{ref: ptr_ref}, offset \\ 0) do
     {ref, size} = Beaver.Native.forward(mod, "make_from_opaque_ptr", [ptr_ref, offset])
-    Beaver.Native.forward(Beaver.Native.F32, "memref_dump", [ref])
     {ref, size}
   end
 end
--- apps/beaver_nx/lib/beaver/nx/defn.ex
+++ apps/beaver_nx/lib/beaver/nx/defn.ex
@@ -460,7 +460,6 @@ defmodule Beaver.Nx.Defn do
     mems =
       Tuple.to_list(tuple)
       |> Enum.map(&memref_from_tensor/1)
-      |> Enum.map(&Beaver.Native.Memory.descriptor_dump/1)
 
     # TODO: support array of memref descriptor of different kinds
     first = mems |> List.first()
