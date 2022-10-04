c1149dabc7c00487c55ea4b7aebeab7843a7d304
tsai <jackalcooper@gmail.com>
2022-10-04 17:59:39 +0800

    move code to prebuilt macro

--- apps/beaver/lib/beaver/mlir/capi.ex
+++ apps/beaver/lib/beaver/mlir/capi.ex
@@ -4,6 +4,18 @@ defmodule Beaver.MLIR.CAPI do
   use Kinda.Prebuilt,
     otp_app: :beaver,
     base_url: "https://github.com/philss/rustler_precompilation_example/releases/download",
+    version: "0.2.4",
+    wrapper: Path.join(File.cwd!(), "native/wrapper.h"),
+    zig_src: "native/mlir-zig",
+    include_paths: %{
+      llvm_include: Beaver.LLVM.Config.include_dir(),
+      beaver_include: Path.join(File.cwd!(), "native/mlir-c/include")
+    },
+    library_paths: %{
+      beaver_libdir: Path.join([Mix.Project.build_path(), "native-install", "lib"])
+    },
+    type_gen: &__MODULE__.CodeGen.type_gen/2,
+    nif_gen: &__MODULE__.CodeGen.nif_gen/1,
     version: "0.2.4"
 
   @moduledoc """
@@ -19,120 +31,6 @@ defmodule Beaver.MLIR.CAPI do
           ["native/mlir-zig/#{Mix.env()}/build.zig"],
       not String.contains?(path, "kinda.gen.zig") do
     @external_resource path
-  end
-
-  # generate Zig and compile it as a NIF library
-  %{
-    nifs: nifs,
-    resource_kinds: resource_kinds,
-    dest_dir: dest_dir,
-    zig_t_module_map: zig_t_module_map
-  } =
-    Kinda.gen(__MODULE__, Path.join(File.cwd!(), "native/wrapper.h"), "native/mlir-zig",
-      include_paths: %{
-        llvm_include: Beaver.LLVM.Config.include_dir(),
-        beaver_include: Path.join(File.cwd!(), "native/mlir-c/include")
-      },
-      library_paths: %{
-        beaver_libdir: Path.join([Mix.Project.build_path(), "native-install", "lib"])
-      },
-      type_gen: &__MODULE__.CodeGen.type_gen/2,
-      nif_gen: &__MODULE__.CodeGen.nif_gen/1,
-      version: "0.2.4"
-    )
-
-  root_module = __MODULE__
-  forward_module = Beaver.Native
-  # generate resource modules
-  for %Kinda.CodeGen.Type{
-        module_name: module_name,
-        zig_t: zig_t,
-        fields: fields
-      } = type <-
-        resource_kinds,
-      Atom.to_string(module_name)
-      |> String.starts_with?(Atom.to_string(__MODULE__)) do
-    Logger.debug("[Beaver] building resource kind #{module_name}")
-
-    defmodule module_name do
-      @moduledoc """
-      #{zig_t}
-      """
-
-      use Kinda.ResourceKind,
-        root_module: root_module,
-        fields: fields,
-        forward_module: forward_module
-    end
-  end
-
-  # generate stubs for generated NIFs
-  Logger.debug("[Beaver] generating NIF wrappers")
-
-  mem_ref_descriptor_kinds =
-    for rank <- [
-          DescriptorUnranked,
-          Descriptor1D,
-          Descriptor2D,
-          Descriptor3D,
-          Descriptor4D,
-          Descriptor5D,
-          Descriptor6D,
-          Descriptor7D,
-          Descriptor8D,
-          Descriptor9D
-        ],
-        t <- [Complex.F32, U8, U16, U32, I8, I16, I32, I64, F32, F64] do
-      %Kinda.CodeGen.Type{
-        module_name: Module.concat([Beaver.Native, t, MemRef, rank]),
-        kind_functions: Beaver.MLIR.CAPI.CodeGen.memref_kind_functions()
-      }
-    end
-
-  extra_kind_nifs =
-    ([
-       %Kinda.CodeGen.Type{
-         module_name: Beaver.Native.PtrOwner
-       },
-       %Kinda.CodeGen.Type{
-         module_name: Beaver.Native.Complex.F32,
-         kind_functions: Beaver.MLIR.CAPI.CodeGen.memref_kind_functions()
-       }
-     ] ++ mem_ref_descriptor_kinds)
-    |> Enum.map(&Kinda.CodeGen.NIF.from_resource_kind/1)
-    |> List.flatten()
-
-  for nif <- nifs ++ extra_kind_nifs do
-    args_ast = Macro.generate_unique_arguments(nif.arity, __MODULE__)
-
-    %Kinda.CodeGen.NIF{wrapper_name: wrapper_name, nif_name: nif_name, ret: ret} = nif
-    @doc false
-    def unquote(nif_name)(unquote_splicing(args_ast)),
-      do:
-        raise(
-          "NIF for resource kind is not implemented, or failed to load NIF library. Function: :\"#{unquote(nif_name)}\"/#{unquote(nif.arity)}"
-        )
-
-    if wrapper_name do
-      if ret == "void" do
-        def unquote(String.to_atom(wrapper_name))(unquote_splicing(args_ast)) do
-          refs = Kinda.unwrap_ref([unquote_splicing(args_ast)])
-          ref = apply(__MODULE__, unquote(nif_name), refs)
-          :ok = unquote(forward_module).check!(ref)
-        end
-      else
-        return_module = Kinda.module_name(ret, forward_module, zig_t_module_map)
-
-        def unquote(String.to_atom(wrapper_name))(unquote_splicing(args_ast)) do
-          refs = Kinda.unwrap_ref([unquote_splicing(args_ast)])
-          ref = apply(__MODULE__, unquote(nif_name), refs)
-
-          struct!(unquote(return_module),
-            ref: unquote(forward_module).check!(ref)
-          )
-        end
-      end
-    end
   end
 
   # stubs for hand-written NIFs
@@ -165,25 +63,5 @@ defmodule Beaver.MLIR.CAPI do
   def beaver_raw_context_attach_diagnostic_handler(_), do: raise("NIF not loaded")
 
   # setup NIF loading
-  @on_load :load_nifs
-
-  @dest_dir dest_dir
-  def load_nifs do
-    nif_path = Path.join(@dest_dir, "lib/libBeaverNIF")
-    dylib = "#{nif_path}.dylib"
-    so = "#{nif_path}.so"
-
-    if File.exists?(dylib) do
-      File.ln_s(dylib, so)
-    end
-
-    Logger.debug("[Beaver] loading NIF")
-
-    with :ok <- :erlang.load_nif(nif_path, 0) do
-      Logger.debug("[Beaver] NIF loaded")
-      :ok
-    else
-      error -> error
-    end
-  end
+  @on_load :kinda_on_load
 end
--- apps/kinda/lib/prebuilt.ex
+++ apps/kinda/lib/prebuilt.ex
@@ -2,6 +2,9 @@ defmodule Kinda.Prebuilt do
   defmacro __using__(opts) do
     force =
       quote do
+        opts = unquote(opts)
+        contents = Kinda.Prebuilt.__using__(__MODULE__, opts)
+        Module.eval_quoted(__MODULE__, contents)
       end
 
     quote do
@@ -57,5 +60,159 @@ defmodule Kinda.Prebuilt do
           raise precomp_error
       end
     end
+  end
+
+  def __using__(root_module, opts) do
+    require Logger
+    wrapper = Keyword.fetch!(opts, :wrapper)
+    zig_src = Keyword.fetch!(opts, :zig_src)
+
+    %{
+      nifs: nifs,
+      resource_kinds: resource_kinds,
+      dest_dir: dest_dir,
+      zig_t_module_map: zig_t_module_map
+    } = Kinda.gen(root_module, wrapper, zig_src, opts)
+
+    forward_module = Beaver.Native
+    # generate resource modules
+    kind_ast =
+      for %Kinda.CodeGen.Type{
+            module_name: module_name,
+            zig_t: zig_t,
+            fields: fields
+          } = type <-
+            resource_kinds,
+          Atom.to_string(module_name)
+          |> String.starts_with?(Atom.to_string(root_module)) do
+        Logger.debug("[Beaver] building resource kind #{module_name}")
+
+        quote bind_quoted: [
+                root_module: root_module,
+                module_name: module_name,
+                zig_t: zig_t,
+                fields: fields,
+                forward_module: forward_module
+              ] do
+          defmodule module_name do
+            @moduledoc """
+            #{zig_t}
+            """
+
+            use Kinda.ResourceKind,
+              root_module: root_module,
+              fields: fields,
+              forward_module: forward_module
+          end
+        end
+      end
+
+    # generate stubs for generated NIFs
+    Logger.debug("[Beaver] generating NIF wrappers")
+
+    mem_ref_descriptor_kinds =
+      for rank <- [
+            DescriptorUnranked,
+            Descriptor1D,
+            Descriptor2D,
+            Descriptor3D,
+            Descriptor4D,
+            Descriptor5D,
+            Descriptor6D,
+            Descriptor7D,
+            Descriptor8D,
+            Descriptor9D
+          ],
+          t <- [Complex.F32, U8, U16, U32, I8, I16, I32, I64, F32, F64] do
+        %Kinda.CodeGen.Type{
+          module_name: Module.concat([Beaver.Native, t, MemRef, rank]),
+          kind_functions: Beaver.MLIR.CAPI.CodeGen.memref_kind_functions()
+        }
+      end
+
+    extra_kind_nifs =
+      ([
+         %Kinda.CodeGen.Type{
+           module_name: Beaver.Native.PtrOwner
+         },
+         %Kinda.CodeGen.Type{
+           module_name: Beaver.Native.Complex.F32,
+           kind_functions: Beaver.MLIR.CAPI.CodeGen.memref_kind_functions()
+         }
+       ] ++ mem_ref_descriptor_kinds)
+      |> Enum.map(&Kinda.CodeGen.NIF.from_resource_kind/1)
+      |> List.flatten()
+
+    nif_ast =
+      for nif <- nifs ++ extra_kind_nifs do
+        args_ast = Macro.generate_unique_arguments(nif.arity, __MODULE__)
+
+        %Kinda.CodeGen.NIF{wrapper_name: wrapper_name, nif_name: nif_name, ret: ret} = nif
+
+        stub_ast =
+          quote do
+            @doc false
+            def unquote(nif_name)(unquote_splicing(args_ast)),
+              do:
+                raise(
+                  "NIF for resource kind is not implemented, or failed to load NIF library. Function: :\"#{unquote(nif_name)}\"/#{unquote(nif.arity)}"
+                )
+          end
+
+        wrapper_ast =
+          if wrapper_name do
+            if ret == "void" do
+              quote do
+                def unquote(String.to_atom(wrapper_name))(unquote_splicing(args_ast)) do
+                  refs = Kinda.unwrap_ref([unquote_splicing(args_ast)])
+                  ref = apply(__MODULE__, unquote(nif_name), refs)
+                  :ok = unquote(forward_module).check!(ref)
+                end
+              end
+            else
+              return_module = Kinda.module_name(ret, forward_module, zig_t_module_map)
+
+              quote do
+                def unquote(String.to_atom(wrapper_name))(unquote_splicing(args_ast)) do
+                  refs = Kinda.unwrap_ref([unquote_splicing(args_ast)])
+                  ref = apply(__MODULE__, unquote(nif_name), refs)
+
+                  struct!(unquote(return_module),
+                    ref: unquote(forward_module).check!(ref)
+                  )
+                end
+              end
+            end
+          end
+
+        [stub_ast, wrapper_ast]
+      end
+      |> List.flatten()
+
+    load_ast =
+      quote do
+        @dest_dir unquote(dest_dir)
+        def kinda_on_load do
+          require Logger
+          nif_path = Path.join(@dest_dir, "lib/libBeaverNIF")
+          dylib = "#{nif_path}.dylib"
+          so = "#{nif_path}.so"
+
+          if File.exists?(dylib) do
+            File.ln_s(dylib, so)
+          end
+
+          Logger.debug("[Beaver] loading NIF, path: #{nif_path}")
+
+          with :ok <- :erlang.load_nif(nif_path, 0) do
+            Logger.debug("[Beaver] NIF loaded, path: #{nif_path}")
+            :ok
+          else
+            error -> error
+          end
+        end
+      end
+
+    kind_ast ++ nif_ast ++ [load_ast]
   end
 end
