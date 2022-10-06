3210debcab679ffaf64a0b6bcbcb7036abbd33fb
tsai <jackalcooper@gmail.com>
2022-10-06 22:33:33 +0800

    update zig build

--- native/mlir-zig/test/build.zig
+++ native/mlir-zig/test/build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const kinda = @import("build.imp.zig");
+const builtin = @import("builtin");
+const os = builtin.os.tag;
 
 pub fn build(b: *std.build.Builder) void {
     b.cache_root = kinda.cache_root;
@@ -12,7 +14,12 @@ pub fn build(b: *std.build.Builder) void {
     lib.addSystemIncludeDir(kinda.llvm_include);
     lib.addSystemIncludeDir(kinda.beaver_include);
     lib.addLibPath(kinda.beaver_libdir);
-    lib.addRPath("@loader_path");
+    if (os == .linux) {
+        lib.addRPath("$ORIGIN");
+    }
+    if (os == .macos) {
+        lib.addRPath("@loader_path");
+    }
     lib.linkSystemLibrary("MLIRBeaver");
     lib.linker_allow_shlib_undefined = true;
     lib.install();
