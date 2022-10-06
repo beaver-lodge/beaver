88042e869eef458ebb064f11b737ac5fb7aa0713
tsai <jackalcooper@gmail.com>
2022-10-06 18:40:41 +0800

    revert changes on test config

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,4 @@
 import Config
 
-# config :beaver, :build_cmake, true
-# config :kinda, :force_build, beaver: true
-config :beaver, :prebuilt_base_url, "http://127.0.0.1:8000/"
+config :beaver, :build_cmake, true
+config :kinda, :force_build, beaver: true
