bb1e1d86a3964452971fedb5b83190fdda7c645a
Shenghang Tsai <jackalcooper@gmail.com>
2023-01-12 23:05:12 +0800

    Refactor Memory.Descriptor by removing `use Kinda.ResourceKind` (#155)

--- lib/beaver/native/memory_descriptor.ex
+++ lib/beaver/native/memory_descriptor.ex
@@ -1,5 +1,18 @@
 defmodule Beaver.Native.Memory.Descriptor do
-  use Kinda.ResourceKind,
-    forward_module: Beaver.Native,
-    generic: true
+  @moduledoc false
+  @type t() :: %__MODULE__{ref: reference(), descriptor_kind: atom()}
+  defstruct [:ref, :descriptor_kind]
+
+  def make(kind, {_allocated, _aligned, _offset, _sizes, _strides} = args)
+      when is_atom(kind) do
+    %__MODULE__{
+      ref:
+        Beaver.Native.forward(
+          kind,
+          "make",
+          Tuple.to_list(args)
+        ),
+      descriptor_kind: kind
+    }
+  end
 end
