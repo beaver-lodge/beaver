a5df256025b11c42de1b3ea3c396e551391e3d0a
tsai <jackalcooper@gmail.com>
2023-06-26 10:08:51 +0800

    native-install -> native_install

--- scripts/build-for-publish.sh
+++ scripts/build-for-publish.sh
@@ -2,9 +2,9 @@ set -e
 export MIX_ENV=prod
 mix compile.elixir_make
 mix compile
-LIB_FINAL_SO=$(ls _build/${MIX_ENV}/lib/beaver/native-install/lib | grep -E "libbeaver.+so")
+LIB_FINAL_SO=$(ls _build/${MIX_ENV}/lib/beaver/native_install/lib | grep -E "libbeaver.+so")
 LIB_FINAL_NAME=${LIB_FINAL_SO}.tar.gz
 tar --dereference -cvzf ${LIB_FINAL_NAME} \
-    -C $PWD/_build/${MIX_ENV}/lib/beaver/native-install/lib $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native-install/lib && ls *.so) \
-    -C $PWD/_build/${MIX_ENV}/lib/beaver/native-install/lib $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native-install/lib && ls *.dylib) \
-    -C $PWD/_build/${MIX_ENV}/lib/beaver/native-install $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native-install && ls *.ex)
+    -C $PWD/_build/${MIX_ENV}/lib/beaver/native_install/lib $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native_install/lib && ls *.so) \
+    -C $PWD/_build/${MIX_ENV}/lib/beaver/native_install/lib $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native_install/lib && ls *.dylib) \
+    -C $PWD/_build/${MIX_ENV}/lib/beaver/native_install $(cd $PWD/_build/${MIX_ENV}/lib/beaver/native_install && ls *.ex)
