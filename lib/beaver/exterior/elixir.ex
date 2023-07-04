b2b2514a489ec50d1b10caf52ecd112bd6f73d8e
tsai <jackalcooper@gmail.com>
2023-07-04 19:34:58 +0800

    minor refactor

--- lib/beaver/exterior/elixir.ex
+++ lib/beaver/exterior/elixir.ex
@@ -2,9 +2,7 @@ defmodule Beaver.Exterior.Elixir do
   alias Beaver.MLIR
   @behaviour Beaver.Exterior
   def register_dialect(ctx) do
-    MLIR.CAPI.mlirDialectHandleRegisterDialect(
-      MLIR.CAPI.mlirGetDialectHandle__elixir__(),
-      ctx
-    )
+    MLIR.CAPI.mlirGetDialectHandle__elixir__()
+    |> MLIR.CAPI.mlirDialectHandleRegisterDialect(ctx)
   end
 end
