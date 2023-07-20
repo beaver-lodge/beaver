ceab3a1af7bdce9ea8b2ff4cb4ea3e5591eded66
Shenghang Tsai <jackalcooper@gmail.com>
2023-07-20 10:37:51 +0800

    Fix LoadIRTest not creating op with MLIR context (#215)

--- test/load_ir_test.exs
+++ test/load_ir_test.exs
@@ -1,13 +1,14 @@
 defmodule LoadIRTest do
-  use ExUnit.Case
-
+  use Beaver.Case, async: true
+  alias Beaver.MLIR
   import Beaver.MLIR.Sigils
 
-  test "example from upstream with br" do
+  test "example from upstream with br", test_context do
     content = File.read!("test/br_example.mlir")
 
     ~m"""
     #{content}
-    """
+    """.(test_context[:ctx])
+    |> MLIR.Operation.verify!()
   end
 end
