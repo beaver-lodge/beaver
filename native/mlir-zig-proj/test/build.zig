929ec9c8a65ea4c432f48c05ae973892dd4d220b
tsai <jackalcooper@gmail.com>
2023-08-04 15:52:53 +0800

    rm build.imp.zig

--- native/mlir-zig-proj/test/build.zig
+++ native/mlir-zig-proj/test/build.zig
@@ -1,14 +1,20 @@
 const std = @import("std");
-const kinda = @import("build.imp.zig");
 const builtin = @import("builtin");
 const os = builtin.os.tag;
 
 pub fn build(b: *std.build.Builder) void {
+    const env_key = "KINDA_LIB_NAME";
+    var lib_name: []u8 = undefined;
+    if (std.process.getEnvVarOwned(b.allocator, env_key)) |v| {
+        lib_name = v;
+    } else |_| {
+        @panic("Env var empty: " ++ env_key);
+    }
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const target: std.zig.CrossTarget = .{};
     const lib = b.addSharedLibrary(.{
-        .name = kinda.lib_name,
+        .name = lib_name,
         .root_source_file = .{ .path = "src/main.zig" },
         .optimize = .Debug,
         .target = target,
