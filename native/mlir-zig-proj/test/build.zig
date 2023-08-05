68dfb6de7e31ec44fb2e1309ada3efbc994cf20a
tsai <jackalcooper@gmail.com>
2023-08-06 03:03:09 +0800

    use -DKINDA_LIB_NAME

--- native/mlir-zig-proj/test/build.zig
+++ native/mlir-zig-proj/test/build.zig
@@ -3,13 +3,12 @@ const builtin = @import("builtin");
 const os = builtin.os.tag;
 
 pub fn build(b: *std.build.Builder) void {
-    const env_key = "KINDA_LIB_NAME";
-    var lib_name: []u8 = undefined;
-    if (std.process.getEnvVarOwned(b.allocator, env_key)) |v| {
+    var lib_name: []const u8 = undefined;
+    const lib_name_key = "KINDA_LIB_NAME";
+    const des = "required -D" ++ lib_name_key;
+    if (b.option([]const u8, lib_name_key, des)) |v| {
         lib_name = v;
-    } else |_| {
-        @panic("Env var empty: " ++ env_key);
-    }
+    } else @panic(des);
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const target: std.zig.CrossTarget = .{};
