e680b609d6e6209d844fb27857669372ada04b5c
tsai <jackalcooper@gmail.com>
2023-11-03 20:25:02 +0800

    add fmt

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,4 @@
-locals_without_parens = [deftype: 1, deftype: 2]
+locals_without_parens = [deftype: 1, deftype: 2, defop: 1, defop: 2, defalias: 1, defalias: 2]
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
