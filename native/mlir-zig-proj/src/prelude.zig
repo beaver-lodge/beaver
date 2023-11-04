053efa4ec3f9476ec3f686d077c25c6d574739ab
tsai <jackalcooper@gmail.com>
2023-11-04 15:00:29 +0800

    add UnmanagedDenseResourceElementsAttrGetDeleteCallback

--- native/mlir-zig-proj/src/prelude.zig
+++ native/mlir-zig-proj/src/prelude.zig
@@ -7,6 +7,7 @@ pub const DiagnosticHandlerDeleteUserData = ?*const fn (?*anyopaque) callconv(.C
 pub const ExternalPassConstruct = ?*const fn (?*anyopaque) callconv(.C) ?*anyopaque;
 pub const ExternalPassRun = ?*const fn (c.MlirOperation, c.MlirExternalPass, ?*anyopaque) callconv(.C) void;
 pub const GenericCallback = ?*const fn (c.MlirContext, ?*anyopaque) callconv(.C) c.MlirLogicalResult;
+pub const UnmanagedDenseResourceElementsAttrGetDeleteCallback = ?*const fn (?*anyopaque, ?*const anyopaque, usize, usize) callconv(.C) void;
 pub const AffineMapCompressUnusedSymbolsPopulateResult = ?*const fn (?*anyopaque, isize, c.MlirAffineMap) callconv(.C) void;
 pub const SymbolTableWalkSymbolTablesCallback = ?*const fn (c.MlirOperation, bool, ?*anyopaque) callconv(.C) void;
 pub usingnamespace c;
