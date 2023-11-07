225a6ea440a20028f1f21fdce7f91b52418a85e7
tsai <jackalcooper@gmail.com>
2023-11-07 12:20:14 +0800

    mv IR gen out

--- test/irdl_test.exs
+++ test/irdl_test.exs
@@ -67,34 +67,6 @@ defmodule IRDLTest do
     """.(test_context[:ctx])
     |> MLIR.Operation.verify!()
 
-    mlir ctx: test_context[:ctx] do
-      module do
-        alias Beaver.MLIR.Dialect.Arith
-        cf32 = ~t{!cmath.complex<f32>}
-        f32 = Type.f(32)
-
-        Func.func conorm(function_type: Type.function([cf32, cf32], [f32])) do
-          region do
-            block bb_entry(p >>> cf32, q >>> cf32) do
-              norm_p = CMath.norm(p) >>> f32
-              norm_q = CMath.norm(q) >>> f32
-              pq = Arith.mulf(norm_p, norm_q) >>> f32
-              Func.return(pq) >>> []
-            end
-          end
-        end
-
-        Func.func conorm2(function_type: Type.function([cf32, cf32], [f32])) do
-          region do
-            block bb_entry(p >>> cf32, q >>> cf32) do
-              pq = CMath.mul(p, q) >>> cf32
-              conorm = CMath.norm(pq) >>> f32
-              Func.return(conorm) >>> []
-            end
-          end
-        end
-      end
-    end
-    |> MLIR.Operation.verify!()
+    CMath.IRExample.gen(test_context[:ctx])
   end
 end
---
+++ test/support/cmath_ir.ex
@@ -0,0 +1,37 @@
+defmodule CMath.IRExample do
+  use Beaver
+  alias Beaver.MLIR.Dialect.Func
+  require Func
+
+  def gen(ctx) do
+    mlir ctx: ctx do
+      module do
+        alias Beaver.MLIR.Dialect.Arith
+        cf32 = ~t{!cmath.complex<f32>}
+        f32 = Type.f(32)
+
+        Func.func conorm(function_type: Type.function([cf32, cf32], [f32])) do
+          region do
+            block bb_entry(p >>> cf32, q >>> cf32) do
+              norm_p = CMath.norm(p) >>> f32
+              norm_q = CMath.norm(q) >>> f32
+              pq = Arith.mulf(norm_p, norm_q) >>> f32
+              Func.return(pq) >>> []
+            end
+          end
+        end
+
+        Func.func conorm2(function_type: Type.function([cf32, cf32], [f32])) do
+          region do
+            block bb_entry(p >>> cf32, q >>> cf32) do
+              pq = CMath.mul(p, q) >>> cf32
+              conorm = CMath.norm(pq) >>> f32
+              Func.return(conorm) >>> []
+            end
+          end
+        end
+      end
+    end
+    |> MLIR.Operation.verify!()
+  end
+end
