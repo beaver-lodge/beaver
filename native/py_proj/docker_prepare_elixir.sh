eef1c696b92d81a11f53ab410380ba97f7651fd8
tsai <jackalcooper@gmail.com>
2023-11-30 11:27:00 +0800

    add docker install

---
+++ native/py_proj/docker_prepare_elixir.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xe
+
+# manylinux prep
+if [[ -f "/etc/centos-release" ]]; then
+  yum install -y epel-release
+  yum makecache --refresh
+  # sometimes the epel server is down. retry 5 times
+  for i in $(seq 1 5); do
+    yum install -y elixir zig && s=0 && break || s=$? && sleep 15
+  done
+
+elif [[ -f "/etc/alpine-release" ]]; then
+  # musllinux prep
+  # ccache already present
+  apk add elixir zig
+fi
