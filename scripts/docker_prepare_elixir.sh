f596b4537435feab4e96cbf14ea04da6b4e986b7
tsai <jackalcooper@gmail.com>
2023-11-30 11:45:32 +0800

    fix

--- scripts/docker_prepare_elixir.sh
+++ scripts/docker_prepare_elixir.sh
@@ -3,9 +3,6 @@ set -xe
 
 # manylinux prep
 if [[ -f "/etc/centos-release" ]]; then
-  yum install -y epel-release
-  yum makecache --refresh
-  # sometimes the epel server is down. retry 5 times
   for i in $(seq 1 5); do
     dnf install -y elixir zig && s=0 && break || s=$? && sleep 15
   done
