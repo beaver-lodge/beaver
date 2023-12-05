53f6473cb24f05072200a5ad3089a17b19283f16
Shenghang Tsai <jackalcooper@gmail.com>
2023-12-05 13:48:56 +0800

    Prototype Elixir AST dialect (#229)
    ## Source
    ```elixir
    defmodule TwoFuncMod do
      def add_two_literal() do
        a = 1 + 2
        b = a + 3
        b
      end
    
      def main() do
        add_two_literal()
      end
    end
    
    defmodule OneFuncMod do
      def add_two_literal() do
        a = 1 + 2
        b = a + 3
        b
      end
    end
    ```
    ## IR generated
    ```mlir
    "builtin.module"() ({
      "ex.mod"() ({
        "ex.func"() ({
          %0 = "ex.lit_int"() {value = 1 : i64} : () -> i64
          %1 = "ex.lit_int"() {value = 2 : i64} : () -> i64
          %2 = "ex.add"(%0, %1) : (i64, i64) -> !ex.dyn
          %3 = "ex.var"() {name = "a"} : () -> !ex.unbound
          %4 = "ex.bind"(%3, %2) : (!ex.unbound, !ex.dyn) -> !ex.bound
          %5 = "ex.var"() {name = "a"} : () -> !ex.dyn
          %6 = "ex.lit_int"() {value = 3 : i64} : () -> i64
          %7 = "ex.add"(%5, %6) : (!ex.dyn, i64) -> !ex.dyn
          %8 = "ex.var"() {name = "b"} : () -> !ex.unbound
          %9 = "ex.bind"(%8, %7) : (!ex.unbound, !ex.dyn) -> !ex.bound
          %10 = "ex.var"() {name = "b"} : () -> !ex.dyn
        }) {name = "add_two_literal"} : () -> ()
        "ex.func"() ({
          %0 = "ex.call"() {name = "add_two_literal"} : () -> !ex.dyn
        }) {name = "main"} : () -> ()
      }) {name = "TwoFuncMod"} : () -> ()
      "ex.mod"() ({
        "ex.func"() ({
          %0 = "ex.lit_int"() {value = 1 : i64} : () -> i64
          %1 = "ex.lit_int"() {value = 2 : i64} : () -> i64
          %2 = "ex.add"(%0, %1) : (i64, i64) -> !ex.dyn
          %3 = "ex.var"() {name = "a"} : () -> !ex.unbound
          %4 = "ex.bind"(%3, %2) : (!ex.unbound, !ex.dyn) -> !ex.bound
          %5 = "ex.var"() {name = "a"} : () -> !ex.dyn
          %6 = "ex.lit_int"() {value = 3 : i64} : () -> i64
          %7 = "ex.add"(%5, %6) : (!ex.dyn, i64) -> !ex.dyn
          %8 = "ex.var"() {name = "b"} : () -> !ex.unbound
          %9 = "ex.bind"(%8, %7) : (!ex.unbound, !ex.dyn) -> !ex.bound
          %10 = "ex.var"() {name = "b"} : () -> !ex.dyn
        }) {name = "add_two_literal"} : () -> ()
      }) {name = "OneFuncMod"} : () -> ()
    }) : () -> ()
    ```

---
+++ test/elixir_ast_dialect_test.exs
@@ -0,0 +1,31 @@
+defmodule ELXDialectTest do
+  use Beaver.Case, async: true
+  alias Beaver.MLIR
+  @moduletag :smoke
+
+  test "gen elx from ast", test_context do
+    quote do
+      defmodule TwoFuncMod do
+        def add_two_literal() do
+          a = 1 + 2
+          b = a + 3
+          b
+        end
+
+        def main() do
+          add_two_literal()
+        end
+      end
+
+      defmodule OneFuncMod do
+        def add_two_literal() do
+          a = 1 + 2
+          b = a + 3
+          b
+        end
+      end
+    end
+    |> ElixirAST.from_ast(ctx: test_context[:ctx])
+    |> MLIR.dump!()
+  end
+end
---
+++ test/support/elixir_ast_dialect.ex
@@ -0,0 +1,138 @@
+defmodule ElixirAST do
+  require Logger
+  @moduledoc "Prototype the Elixir AST dialect."
+  use Beaver.Slang, name: "ex"
+  deftype dyn
+  deftype bound
+  deftype unbound
+  defop mod(), do: []
+  defop func(), do: []
+  defop lit_int(), do: [Type.i64()]
+  defop var(), do: []
+  defop bind(), do: []
+  defop call(), do: []
+  defop add(), do: []
+  alias Beaver.MLIR.Type
+  alias Beaver.MLIR.Attribute
+  use Beaver
+
+  defp gen_mlir(
+         {:defmodule, _,
+          [
+            {:__aliases__, [alias: false], [name]},
+            [
+              do: do_body
+            ]
+          ]},
+         ctx,
+         block
+       ) do
+    mlir ctx: ctx, block: block do
+      __MODULE__.mod name: "\"#{name}\"" do
+        region do
+          block functions do
+            Macro.prewalk(do_body, &gen_mlir(&1, ctx, Beaver.Env.block()))
+          end
+        end
+      end >>>
+        []
+    end
+  end
+
+  defp gen_mlir(
+         {:def, _,
+          [
+            {name, _, _},
+            [
+              do: do_body
+            ]
+          ]},
+         ctx,
+         block
+       ) do
+    mlir ctx: ctx, block: block do
+      __MODULE__.func name: "\"#{name}\"" do
+        region do
+          block func_body do
+            Macro.prewalk(do_body, &gen_mlir(&1, ctx, Beaver.Env.block()))
+          end
+        end
+      end >>>
+        []
+    end
+  end
+
+  defp gen_mlir(
+         {:=, [],
+          [
+            {name, [], _},
+            bound
+          ]},
+         ctx,
+         block
+       ) do
+    mlir ctx: ctx, block: block do
+      bound = Macro.prewalk(bound, &gen_mlir(&1, ctx, block))
+      var = __MODULE__.var(name: "\"#{name}\"") >>> __MODULE__.unbound()
+      __MODULE__.bind(var, bound) >>> __MODULE__.bound()
+    end
+  end
+
+  defp gen_mlir(
+         {:+, _, [left, right]},
+         ctx,
+         block
+       ) do
+    mlir ctx: ctx, block: block do
+      [left, right] =
+        for i <- [left, right] do
+          Macro.prewalk(i, &gen_mlir(&1, ctx, block))
+        end
+
+      __MODULE__.add(left, right) >>> __MODULE__.dyn()
+    end
+  end
+
+  defp gen_mlir({name, [], mod}, ctx, block) when is_atom(name) and is_atom(mod) do
+    mlir ctx: ctx, block: block do
+      __MODULE__.var(name: "\"#{name}\"") >>> __MODULE__.dyn()
+    end
+  end
+
+  defp gen_mlir({name, [], [] = args}, ctx, block) when is_atom(name) do
+    mlir ctx: ctx, block: block do
+      __MODULE__.call(args, name: "\"#{name}\"") >>> __MODULE__.dyn()
+    end
+  end
+
+  defp gen_mlir(i, ctx, block) when is_integer(i) do
+    mlir ctx: ctx, block: block do
+      __MODULE__.lit_int(value: Attribute.integer(Type.i64(), i)) >>> Type.i64()
+    end
+  end
+
+  defp gen_mlir({:__block__, _, _} = ast, _, _) do
+    ast
+  end
+
+  defp gen_mlir(ast, _, _) do
+    ast = inspect(ast, pretty: true)
+    Logger.info("AST ignored:\n#{ast}")
+    ast
+  end
+
+  def from_ast(ast, opts) do
+    Beaver.Deferred.from_opts(
+      opts,
+      fn ctx ->
+        Beaver.Slang.load(ctx, __MODULE__)
+
+        mlir ctx: ctx do
+          module do
+            ast |> Macro.prewalk(&gen_mlir(&1, ctx, Beaver.Env.block()))
+          end
+        end
+      end
+    )
+  end
+end
