78159b87ef77b6e46013b671f7ee33c874a14dba
tsai <jackalcooper@gmail.com>
2023-12-13 14:52:30 +0800

    add more cases

--- test/support/region_dialect.ex
+++ test/support/region_dialect.ex
@@ -2,5 +2,6 @@ defmodule TestRegion do
   @moduledoc "An example to showcase the a region dialect in IRDL test."
   use Beaver.Slang, name: "test_region"
   defop any_region(i = {:single, Type.i32()}), do: [], regions: [:any]
-  defop sized_region(), regions: [{:sized, 11}]
+  defop any_region2(i = {:single, Type.i32()}), do: [], regions: [:any, :any]
+  defop sized_region(), regions: {:sized, 11}
 end
