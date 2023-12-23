7a5d3a2ff39ac3b7316308e4b844d066f38d3759
tsai <jackalcooper@gmail.com>
2023-12-23 21:48:07 +0800

    update URL

--- lib/beaver/mlir/capi.ex
+++ lib/beaver/mlir/capi.ex
@@ -25,7 +25,7 @@ defmodule Beaver.MLIR.CAPI do
       Application.compile_env(
         :beaver,
         :prebuilt_base_url,
-        "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2023-08-08-0309"
+        "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2023-12-23-1343"
       ),
     version: "0.3.1",
     wrapper: Path.join(mlir_c_path, "include/mlir-c/Beaver/wrapper.h"),
