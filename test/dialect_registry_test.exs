81f0a111a639df6497c31b3f2263954bdb499d48
Shenghang Tsai <jackalcooper@gmail.com>
2023-12-25 10:06:06 +0800

    Update test case of dialect registry for latest MLIR (#251)

--- test/dialect_registry_test.exs
+++ test/dialect_registry_test.exs
@@ -8,58 +8,61 @@ defmodule DialectRegistryTest do
     assert not Enum.empty?(Dialect.Registry.ops("arith"))
     assert Dialect.Registry.ops("cf") == ["switch", "cond_br", "br", "assert"]
 
-    tuples =
+    registered =
       for d <- Dialect.Registry.dialects(full: true) do
         {d, Dialect.Registry.normalize_dialect_name(d)}
       end
-      |> Enum.sort()
+      |> MapSet.new()
 
-    assert tuples ==
-             Enum.sort([
-               {"acc", "ACC"},
-               {"affine", "Affine"},
-               {"amdgpu", "AMDGPU"},
-               {"amx", "AMX"},
-               {"arith", "Arith"},
-               {"arm_neon", "ArmNeon"},
-               {"arm_sme", "ArmSME"},
-               {"arm_sve", "ArmSVE"},
-               {"async", "Async"},
-               {"bufferization", "Bufferization"},
-               {"builtin", "Builtin"},
-               {"cf", "CF"},
-               {"complex", "Complex"},
-               {"dlti", "DLTI"},
-               {"emitc", "EmitC"},
-               {"func", "Func"},
-               {"gpu", "GPU"},
-               {"index", "Index"},
-               {"irdl", "IRDL"},
-               {"linalg", "Linalg"},
-               {"llvm", "LLVM"},
-               {"math", "Math"},
-               {"memref", "MemRef"},
-               {"mesh", "Mesh"},
-               {"ml_program", "MLProgram"},
-               {"nvgpu", "NVGPU"},
-               {"nvvm", "NVVM"},
-               {"omp", "OMP"},
-               {"pdl", "PDL"},
-               {"pdl_interp", "PDLInterp"},
-               {"quant", "Quant"},
-               {"rocdl", "ROCDL"},
-               {"scf", "SCF"},
-               {"shape", "Shape"},
-               {"sparse_tensor", "SparseTensor"},
-               {"spirv", "SPIRV"},
-               {"tensor", "Tensor"},
-               {"test_dyn", "TestDyn"},
-               {"tosa", "TOSA"},
-               {"transform", "Transform"},
-               {"ub", "Ub"},
-               {"vector", "Vector"},
-               {"x86vector", "X86Vector"}
-             ])
+    expected =
+      [
+        {"acc", "ACC"},
+        {"affine", "Affine"},
+        {"amdgpu", "AMDGPU"},
+        {"amx", "AMX"},
+        {"arith", "Arith"},
+        {"arm_neon", "ArmNeon"},
+        {"arm_sme", "ArmSME"},
+        {"arm_sve", "ArmSVE"},
+        {"async", "Async"},
+        {"bufferization", "Bufferization"},
+        {"builtin", "Builtin"},
+        {"cf", "CF"},
+        {"complex", "Complex"},
+        {"dlti", "DLTI"},
+        {"emitc", "EmitC"},
+        {"func", "Func"},
+        {"gpu", "GPU"},
+        {"index", "Index"},
+        {"irdl", "IRDL"},
+        {"linalg", "Linalg"},
+        {"llvm", "LLVM"},
+        {"math", "Math"},
+        {"memref", "MemRef"},
+        {"mesh", "Mesh"},
+        {"ml_program", "MLProgram"},
+        {"nvgpu", "NVGPU"},
+        {"nvvm", "NVVM"},
+        {"omp", "OMP"},
+        {"pdl", "PDL"},
+        {"pdl_interp", "PDLInterp"},
+        {"quant", "Quant"},
+        {"rocdl", "ROCDL"},
+        {"scf", "SCF"},
+        {"shape", "Shape"},
+        {"sparse_tensor", "SparseTensor"},
+        {"spirv", "SPIRV"},
+        {"tensor", "Tensor"},
+        {"tosa", "TOSA"},
+        {"transform", "Transform"},
+        {"ub", "Ub"},
+        {"vector", "Vector"},
+        {"x86vector", "X86Vector"}
+      ]
+      |> MapSet.new()
+
+    assert MapSet.difference(registered, expected) == MapSet.new([])
+    assert MapSet.difference(expected, registered) == MapSet.new([])
   end
 
   test "ops are unique" do
