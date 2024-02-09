784413bb57ef99017f2e39deedb6b82cd83bf1b3
tsai <jackalcooper@gmail.com>
2024-02-09 11:03:43 +0800

    add more branching

--- test/ast_ir.exs
+++ test/ast_ir.exs
@@ -11,7 +11,7 @@ op dialect.op2()["other attribute": 42 :: i64] ::
   _ ->
     op dialect.inner_op2() :: {}
     op dialect.inner_op3(var._0, var._2, var._3) :: {}
-    br bb1(var._0)
+    br bb2(var._0)
 
   bb1(arg :: i32) ->
     op dialect.inner_op4(arg)[attribute_name2: 42 :: i32] :: {}
@@ -20,4 +20,5 @@ op dialect.op2()["other attribute": 42 :: i64] ::
   bb2(arg2 :: i64) ->
     op dialect.inner_op5(arg2) :: {}
     op "dialect.inner_op5-1", [], [], {}
+    br bb1(var._0)
 end
