c86b6e17bdd2456eb39535a92762797daad648ac
tsai <jackalcooper@gmail.com>
2024-02-14 17:38:32 +0800

    fix wording

--- lib/beaver/diagnostic/server.ex
+++ lib/beaver/diagnostic/server.ex
@@ -18,7 +18,7 @@ defmodule Beaver.Diagnostic.Server do
   end
 
   def flush(_) do
-    raise "To flush diagnostics, you need to create the MLIR context with a diagnostic server. To use the built-in one: 'MLIR.Context.create(diagnostic: :server)'"
+    raise "To flush diagnostic, you need to create the MLIR context with a diagnostic server. To use the built-in one: 'MLIR.Context.create(diagnostic: :server)'"
   end
 
   def handle_call(:flush, _from, state) do
