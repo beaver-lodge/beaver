81ad29b70cc5b7ca0d6ee7247fc123bda560ce5b
tsai <jackalcooper@gmail.com>
2024-02-14 11:07:41 +0800

    simplify example in readme

--- test/gen_ir_test.exs
+++ test/gen_ir_test.exs
@@ -10,28 +10,7 @@ defmodule CFTest do
     ir =
       mlir ctx: test_context[:ctx] do
         module do
-          Func.func some_func(function_type: Type.function([], [Type.i(32)])) do
-            region do
-              block _bb_entry() do
-                v0 = Arith.constant(value: Attribute.integer(Type.i(32), 0)) >>> Type.i(32)
-                cond0 = Arith.constant(true) >>> Type.i(1)
-                CF.cond_br(cond0, Beaver.Env.block(bb1), {Beaver.Env.block(bb2), [v0]}) >>> []
-              end
-
-              block bb1() do
-                v1 = Arith.constant(value: Attribute.integer(Type.i(32), 0)) >>> Type.i(32)
-                _add = Arith.addi(v0, v0) >>> Type.i(32)
-                CF.br({Beaver.Env.block(bb2), [v1]}) >>> []
-              end
-
-              block bb2(arg >>> Type.i(32)) do
-                v2 = Arith.constant(value: Attribute.integer(Type.i(32), 0)) >>> Type.i(32)
-                add = Arith.addi(arg, v2) >>> Type.i(32)
-                Func.return(add) >>> []
-              end
-            end
-          end
-          |> MLIR.Operation.verify!(debug: true)
+          unquote(File.read!("test/readme_example.exs") |> Code.string_to_quoted!())
 
           Func.func some_func2(function_type: Type.function([], [Type.i(32)])) do
             region do
