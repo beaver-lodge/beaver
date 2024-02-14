61851ed05c455b635691d5bd773d764330ab378c
tsai <jackalcooper@gmail.com>
2024-02-14 17:33:28 +0800

    add more tests in region test

--- test/region_test.exs
+++ test/region_test.exs
@@ -1,5 +1,5 @@
 defmodule RegionTest do
-  use Beaver.Case
+  use Beaver.Case, diagnostic: :server
   use Beaver
   alias Beaver.MLIR
   alias Beaver.MLIR.{Attribute, Type}
@@ -87,5 +87,46 @@ defmodule RegionTest do
       end
     end
     |> MLIR.Operation.verify!()
+  end
+
+  test "dangling block", test_context do
+    mlir ctx: test_context[:ctx] do
+      module do
+        Func.func some_func(function_type: Type.function([], [Type.i(32)])) do
+          region do
+            block _bb_entry() do
+              v0 = Arith.constant(value: Attribute.integer(Type.i(32), 0)) >>> Type.i(32)
+              CF.br({Beaver.Env.block(_bb1), [v0]}) >>> []
+            end
+          end
+        end
+      end
+    end
+    |> MLIR.Operation.verify()
+
+    assert Beaver.Diagnostic.Server.flush(test_context[:ctx]) =~
+             "reference to block defined in another region"
+  end
+
+  test "successor of wrong arg type", test_context do
+    mlir ctx: test_context[:ctx] do
+      module do
+        Func.func some_func(function_type: Type.function([], [Type.i(32)])) do
+          region do
+            block _bb_entry() do
+              v0 = Arith.constant(value: Attribute.integer(Type.i(32), 0)) >>> Type.i(32)
+              CF.br({Beaver.Env.block(bb1), [v0]}) >>> []
+            end
+
+            block bb1() do
+            end
+          end
+        end
+      end
+    end
+    |> MLIR.Operation.verify()
+
+    assert Beaver.Diagnostic.Server.flush(test_context[:ctx]) =~
+             "branch has 1 operands for successor"
   end
 end
