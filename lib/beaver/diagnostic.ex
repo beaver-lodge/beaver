d0bacf5c4a3a8fcc036e275d541b8ff33331f5e1
tsai <jackalcooper@gmail.com>
2024-02-14 21:28:51 +0800

    Update diagnostic.ex

--- lib/beaver/diagnostic.ex
+++ lib/beaver/diagnostic.ex
@@ -3,11 +3,8 @@ defmodule Beaver.Diagnostic do
 
   def attach(%MLIR.Context{} = ctx, handler \\ :stderr)
       when is_pid(handler) or handler == :stderr do
-    ref =
-      MLIR.CAPI.beaver_raw_context_attach_diagnostic_handler(ctx.ref, handler)
-      |> Beaver.Native.check!()
-
-    %MLIR.CAPI.MlirDiagnosticHandlerID{ref: ref}
+    MLIR.CAPI.beaver_raw_context_attach_diagnostic_handler(ctx.ref, handler)
+    |> Beaver.Native.check!()
   end
 
   def detach(%MLIR.Context{} = ctx, handler_id) do
