ccc3052d6011656faf8b544c0943cfc15161997e
tsai <jackalcooper@gmail.com>
2024-02-14 21:17:11 +0800

    fix crash

--- test/support/beaver_case.ex
+++ test/support/beaver_case.ex
@@ -13,30 +13,28 @@ defmodule Beaver.Case do
       setup do
         ctx = MLIR.Context.create(unquote(options))
 
-        ds_pid =
+        {server, handler_id} =
           case unquote(options)[:diagnostic] do
             :server ->
               {:ok, pid} = GenServer.start(Beaver.Diagnostic.Server, [])
               id = Beaver.Diagnostic.attach(ctx, pid)
-
-              on_exit(fn ->
-                # Beaver.Diagnostic.detach(ctx, id)
-                :ok = GenServer.stop(pid)
-              end)
-
-              pid
+              {pid, id}
 
             _ ->
               id = Beaver.Diagnostic.attach(ctx, :stderr)
-              # on_exit(fn -> Beaver.Diagnostic.detach(ctx, id) end)
-              nil
+              {nil, id}
           end
 
         on_exit(fn ->
+          if server do
+            :ok = GenServer.stop(server)
+          end
+
+          Beaver.Diagnostic.detach(ctx, handler_id)
           MLIR.Context.destroy(ctx)
         end)
 
-        [ctx: ctx, diagnostic_server: ds_pid]
+        [ctx: ctx, diagnostic_server: server]
       end
     end
   end
