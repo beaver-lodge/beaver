27cb63b43dfdf6f29adf5a2c0ba5961372c55fc3
tsai <jackalcooper@gmail.com>
2024-02-16 20:36:35 +0800

    Update kinda_library.zig

--- native/mlir-zig-proj/src/kinda_library.zig
+++ native/mlir-zig-proj/src/kinda_library.zig
@@ -45,9 +45,7 @@ pub fn KindaLibrary(comptime Kinds: anytype, comptime NIFs: anytype) type {
                         11 => struct { P[0].type.?, P[1].type.?, P[2].type.?, P[3].type.?, P[4].type.?, P[5].type.?, P[6].type.?, P[7].type.?, P[8].type.?, P[9].type.?, P[10].type.? },
                         12 => struct { P[0].type.?, P[1].type.?, P[2].type.?, P[3].type.?, P[4].type.?, P[5].type.?, P[6].type.?, P[7].type.?, P[8].type.?, P[9].type.?, P[10].type.?, P[11].type.? },
                         13 => struct { P[0].type.?, P[1].type.?, P[2].type.?, P[3].type.?, P[4].type.?, P[5].type.?, P[6].type.?, P[7].type.?, P[8].type.?, P[9].type.?, P[10].type.?, P[11].type.?, P[12].type.? },
-                        else => {
-                            @compileError("too many args");
-                        },
+                        else => @compileError("too many args"),
                     };
                 }
                 inline fn variadic_call(args: anytype) FTI.return_type.? {
