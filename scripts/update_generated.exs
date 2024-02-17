bd16c2d77c1838d1b0f4769fa2176d3ccc242e45
tsai <jackalcooper@gmail.com>
2024-02-17 18:02:49 +0800

    add codegen

--- scripts/update_generated.exs
+++ scripts/update_generated.exs
@@ -10,12 +10,47 @@ defmodule Updater do
     zig_ast =
       Zig.Parser.parse(translate_out).code
 
-    for {:fn, %Zig.Parser.FnOptions{extern: true, inline: inline}, parts} = f <-
-          zig_ast,
-        inline != true do
-      {parts[:name], parts[:type], parts[:params] |> length()}
-    end
-    |> dbg
+    functions =
+      for {:fn, %Zig.Parser.FnOptions{extern: true, inline: inline}, parts} <-
+            zig_ast,
+          inline != true do
+        {parts[:name], parts[:type], parts[:params] |> length()}
+      end
+      |> Enum.sort()
+
+    functions =
+      for {name, _ret, _arity} <- functions do
+        lazy_fns = ~w{mlirPassManagerRunOnOp} |> Enum.map(&String.to_atom/1)
+
+        if name in lazy_fns do
+          ~s{L(K, c, "#{name}"),}
+        else
+          ~s{N(K, c, "#{name}"),}
+        end
+      end
+      |> Enum.join("\n")
+
+    txt = """
+    pub const c = @import("prelude.zig");
+    const kl = @import("kinda_library.zig");
+    const e = @import("erl_nif");
+    const nifPrefix = "Elixir.Beaver.MLIR.CAPI.";
+    pub fn N(comptime Kinds: anytype, c_: anytype, comptime name: anytype) e.ErlNifFunc {
+    return kl.KindaNIF(Kinds, c_, name, .{ .overwrite = nifPrefix ++ name });
+    }
+    pub fn L(comptime Kinds: anytype, c_: anytype, comptime name: anytype) e.ErlNifFunc {
+    return kl.KindaNIF(Kinds, c_, name, .{ .flags = 1, .overwrite = nifPrefix ++ name });
+    }
+    const mlir_capi = @import("mlir_capi.zig");
+    const K = mlir_capi.allKinds;
+    pub const nif_entries = .{
+    #{functions}
+    };
+    """
+
+    dst = "native/mlir-zig-proj/src/wrapper.zig"
+    File.write(dst, txt)
+    {_, 0} = System.cmd("zig", ["fmt", dst])
   end
 end
 
