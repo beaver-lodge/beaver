3133bec67cb060c808435514f628f70bbf0d4069
tsai <jackalcooper@gmail.com>
2024-02-19 15:58:37 +0800

    Update build.zig

--- native/mlir-zig-proj/build.zig
+++ native/mlir-zig-proj/build.zig
@@ -18,7 +18,7 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = .ReleaseSmall,
         .target = target,
     });
-    const cflags = [_][]const u8{ "-std=c++17", "-fno-sanitize=undefined" };
+    const cflags = [_][]const u8{ "-std=c++17", "-fno-sanitize=undefined", "-D_GLIBCXX_USE_CXX11_ABI=1" };
     lib.linkLibCpp();
     const cppFiles = .{"mlir-c/lib/CAPI/Beaver.cpp"};
     inline for (cppFiles) |f| {
