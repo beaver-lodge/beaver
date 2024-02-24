ed37b726e9e442d5855c0321ebaab89d679d0dbb
Shenghang Tsai <jackalcooper@gmail.com>
2024-02-24 18:10:02 +0800

    Create relative dylib symlink (#276)

--- lib/beaver/mlir/capi.ex
+++ lib/beaver/mlir/capi.ex
@@ -10,7 +10,9 @@ defmodule Beaver.MLIR.CAPI do
     nif_file = ~c"#{:code.priv_dir(:beaver)}/lib/libBeaverNIF"
 
     if File.exists?(dylib = "#{nif_file}.dylib") do
-      File.ln_s(dylib, "#{nif_file}.so")
+      dylib
+      |> Path.basename()
+      |> File.ln_s("#{nif_file}.so")
     end
 
     case :erlang.load_nif(nif_file, 0) do
