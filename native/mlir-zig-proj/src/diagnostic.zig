3d921fab0d6ccf8fef4dcc8652f9bbaae70bd77a
tsai <jackalcooper@gmail.com>
2024-04-23 16:47:03 +0800

    print_fragment

--- native/mlir-zig-proj/src/diagnostic.zig
+++ native/mlir-zig-proj/src/diagnostic.zig
@@ -8,23 +8,22 @@ const e = @import("erl_nif");
 const result = @import("result.zig");
 
 const BeaverDiagnostic = struct {
-    handler: beam.pid,
-    fn printToMsg(str: anytype, userData: *@This()) void {
-        const env = e.enif_alloc_env() orelse unreachable;
-        defer e.enif_clear_env(env);
-        const msg = beam.make_slice(env, str);
-        _ = beam.send(env, userData.*.handler, msg);
-    }
-    fn printSlice(str: anytype, userData: ?*anyopaque) void {
+    handler: ?beam.pid = null,
+    fn print_fragment(str: anytype, userData: ?*anyopaque) void {
         const ud: ?*@This() = @ptrCast(@alignCast(userData));
         if (ud) |ptr| {
-            printToMsg(str, ptr);
-        } else {
-            stderr.print("{s}", .{str}) catch return;
+            if (ptr.*.handler) |h| {
+                const env = e.enif_alloc_env() orelse unreachable;
+                const msg = beam.make_slice(env, str);
+                defer e.enif_clear_env(env);
+                _ = beam.send(env, h, msg);
+                return;
+            }
         }
+        stderr.print("{s}", .{str}) catch return;
     }
     pub fn printDiagnostic(str: mlir_capi.StringRef.T, userData: ?*anyopaque) callconv(.C) void {
-        printSlice(str.data[0..str.length], userData);
+        print_fragment(str.data[0..str.length], userData);
     }
     pub fn deleteUserData(userData: ?*anyopaque) callconv(.C) void {
         const ud: ?*@This() = @ptrCast(@alignCast(userData));
@@ -33,13 +32,13 @@ const BeaverDiagnostic = struct {
         }
     }
     pub fn errorHandler(diagnostic: c.MlirDiagnostic, userData: ?*anyopaque) callconv(.C) mlir_capi.LogicalResult.T {
-        printSlice("[Beaver] [Diagnostic] [", userData);
+        print_fragment("[Beaver] [Diagnostic] [", userData);
         const loc = c.mlirDiagnosticGetLocation(diagnostic);
         c.beaverLocationPrint(loc, printDiagnostic, userData);
-        printSlice("] ", userData);
+        print_fragment("] ", userData);
 
         c.mlirDiagnosticPrint(diagnostic, printDiagnostic, userData);
-        printSlice("\n", userData);
+        print_fragment("\n", userData);
 
         const num_note = c.mlirDiagnosticGetNumNotes(diagnostic);
         var i: isize = 0;
@@ -48,18 +47,14 @@ const BeaverDiagnostic = struct {
             c.mlirDiagnosticPrint(note_d, printDiagnostic, userData);
             i += 1;
         }
-        printSlice("\n", userData);
+        print_fragment("\n", userData);
         return c.mlirLogicalResultSuccess();
     }
 };
 
 fn do_attach(env: beam.env, _: c_int, args: [*c]const beam.term) !beam.term {
-    var handler: ?beam.pid = beam.get_pid(env, args[1]) catch null;
-    var userData: ?*BeaverDiagnostic = null;
-    if (handler) |h| {
-        userData = try beam.allocator.create(BeaverDiagnostic);
-        userData.?.handler = h;
-    }
+    var userData: ?*BeaverDiagnostic = try beam.allocator.create(BeaverDiagnostic);
+    userData.?.handler = beam.get_pid(env, args[1]) catch null;
     const id = c.mlirContextAttachDiagnosticHandler(try mlir_capi.Context.resource.fetch(env, args[0]), BeaverDiagnostic.errorHandler, userData, BeaverDiagnostic.deleteUserData);
     return try mlir_capi.DiagnosticHandlerID.resource.make(env, id);
 }
