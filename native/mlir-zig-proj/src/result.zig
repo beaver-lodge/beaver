f7e5a9eb227c729b1df25422f94f4798968a624e
tsai <jackalcooper@gmail.com>
2024-04-26 16:51:36 +0800

    Update result.zig

--- native/mlir-zig-proj/src/result.zig
+++ native/mlir-zig-proj/src/result.zig
@@ -1,16 +1,15 @@
 const beam = @import("beam");
 const e = @import("erl_nif");
+const std = @import("std");
 
 pub fn nif(comptime name: [*c]const u8, comptime arity: usize, comptime f: anytype) type {
     return struct {
         fn exported(env: beam.env, n: c_int, args: [*c]const beam.term) callconv(.C) beam.term {
-            var ret: beam.term = undefined;
             if (f(env, n, args)) |r| {
-                ret = r;
+                return r;
             } else |err| {
-                ret = beam.make_error_term(env, beam.make_slice(env, @errorName(err)));
+                return e.enif_raise_exception(env, beam.make_atom(env, @errorName(err)));
             }
-            return ret;
         }
         pub const entry = e.ErlNifFunc{ .name = name, .arity = arity, .fptr = exported, .flags = 0 };
     };
