5d1977b25099df9e3f8162968fbc5eca41b392fc
tsai <jackalcooper@gmail.com>
2024-04-26 13:52:07 +0800

    Delete descriptor.ex

--- lib/beaver/mlir/memref/descriptor.ex
+++
@@ -1,7 +0,0 @@
-defmodule Beaver.MLIR.MemRef.Descriptor do
-  @doc """
-  Allocate memory base on the shape and element type information. A new MemRef descriptor resource is created and returned. The resource will own the allocated memory and will be automatically deallocated when the resource is garbage collected.
-  """
-  def allocate() do
-  end
-end
