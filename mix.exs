849e136347dbac39eae788f3a0764f60cccb4bb1
tsai <jackalcooper@gmail.com>
2024-05-23 13:47:52 +0800

    Release version 0.3.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beaver.MixProject do
   def project do
     [
       app: :beaver,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
