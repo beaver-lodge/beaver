ed7ab1235c33c05bcfd64a7d731fa9d0343231c4
Shenghang Tsai <jackalcooper@gmail.com>
2024-06-20 10:44:07 +0800

    Simplify dialect registering and loading when creating a context (#319)

--- lib/beaver/mlir/context.ex
+++ lib/beaver/mlir/context.ex
@@ -6,33 +6,29 @@ defmodule Beaver.MLIR.Context do
   import MLIR.CAPI
   use Kinda.ResourceKind, forward_module: Beaver.Native
 
-  defp append_all_dialects(ctx) do
+  # create an interim registry and append all dialects to the context
+  defp load_all_dialects(ctx) do
     registry = mlirDialectRegistryCreate()
     mlirRegisterAllDialects(registry)
     mlirContextAppendDialectRegistry(ctx, registry)
+    Beaver.Exterior.register_all(ctx)
     mlirDialectRegistryDestroy(registry)
+    mlirContextLoadAllAvailableDialects(ctx)
   end
 
   @type context_option :: {:allow_unregistered, boolean()} | {:all_dialects, boolean()}
   @spec create(context_option()) :: __MODULE__.t()
   @default_context_option [allow_unregistered: false, all_dialects: true]
   @doc """
-  create a MLIR context, it registers all dialects by default
+  Create a MLIR context. By default it registers and loads all dialects.
   """
   def create(opts \\ @default_context_option) do
     allow_unregistered = opts[:allow_unregistered] || @default_context_option[:allow_unregistered]
     all_dialects = opts[:all_dialects] || @default_context_option[:all_dialects]
-    ctx = mlirContextCreate()
 
-    if all_dialects do
-      append_all_dialects(ctx)
-    end
-
-    mlirContextSetAllowUnregisteredDialects(ctx, Beaver.Native.Bool.make(allow_unregistered))
-    Beaver.Exterior.register_all(ctx)
-    # TODO: do not load dialects twice
-    mlirContextLoadAllAvailableDialects(ctx)
-    ctx
+    mlirContextCreate()
+    |> tap(fn ctx -> if all_dialects, do: load_all_dialects(ctx) end)
+    |> tap(&mlirContextSetAllowUnregisteredDialects(&1, allow_unregistered))
   end
 
   defdelegate destroy(ctx), to: MLIR.CAPI, as: :mlirContextDestroy
