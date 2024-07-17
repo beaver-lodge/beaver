14942401c06229a0697eda3e4abf6b21a2cd52f8
tsai <jackalcooper@gmail.com>
2024-07-17 17:55:00 +0800

    check popped attr

--- test/op_test.exs
+++ test/op_test.exs
@@ -16,9 +16,19 @@ defmodule OpTest do
 
     new_attr_str1 = "dense<1> : vector<4xi32>"
     new_attr_str2 = "dense<2> : vector<4xi32>"
-    get_and_update_in(constant[:value], &{&1, ~a{#{new_attr_str1}}})
+    old_attr = constant[:value]
+    {attr, op} = get_and_update_in(constant[:value], &{&1, ~a{#{new_attr_str1}}})
+    assert MLIR.equal?(attr, old_attr)
+    assert MLIR.equal?(op, constant)
     assert constant |> MLIR.to_string() =~ new_attr_str1
-    get_and_update_in(constant[:value], &{&1, ~a{#{new_attr_str2}}.(test_context[:ctx])})
+
+    old_attr = constant[:value]
+
+    {attr, op} =
+      get_and_update_in(constant[:value], &{&1, ~a{#{new_attr_str2}}.(test_context[:ctx])})
+
+    assert MLIR.equal?(attr, old_attr)
+    assert MLIR.equal?(op, constant)
     assert constant |> MLIR.to_string() =~ new_attr_str2
   end
 end
