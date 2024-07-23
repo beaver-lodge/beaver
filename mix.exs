20a04c56e267f2652c8d04dadc923423b2bbc439
tsai <jackalcooper@gmail.com>
2024-07-23 13:59:00 +0800

    Use relative file path in build.zig.zon

--- mix.exs
+++ mix.exs
@@ -118,10 +118,7 @@ defmodule Beaver.MixProject do
     [
       {:elixir_make, "~> 0.4", runtime: false},
       {:llvm_config, "~> 0.1.0"},
-      if(p = System.get_env("BEAVER_KINDA_PATH"),
-        do: {:kinda, path: p},
-        else: {:kinda, "~> 0.8.1"}
-      ),
+      {:kinda, "~> 0.9.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev}
     ]
