aa45ab829c34f37230864084063dfc310716317f
tsai <jackalcooper@gmail.com>
2024-09-03 10:06:46 +0800

    Upgrade to kinda 0.9.1

--- mix.exs
+++ mix.exs
@@ -111,7 +111,7 @@ defmodule Beaver.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:kinda, "~> 0.9.0"},
+      {:kinda, "~> 0.9.1"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :dev}
     ]
