2d9818a07f18837a932055ac5c62430ca50749cc
tsai <jackalcooper@gmail.com>
2024-09-20 13:31:50 +0800

    Update pass_manager.ex

--- lib/beaver/mlir/pass_manager.ex
+++ lib/beaver/mlir/pass_manager.ex
@@ -4,22 +4,22 @@ defmodule Beaver.MLIR.PassManager do
 
   alias Beaver.MLIR
 
-  @type print_opts :: [
-          before_all: boolean(),
-          after_all: boolean(),
-          module_scope: boolean(),
-          after_only_on_change: boolean(),
-          after_only_on_failure: boolean()
-        ]
-  @spec enable_ir_printing(MLIR.PassManager.t(), print_opts) :: :ok
-  def enable_ir_printing(pm, opts \\ []) do
+  @type print_opt ::
+          {:before_all, boolean()}
+          | {:after_all, boolean()}
+          | {:module_scope, boolean()}
+          | {:after_only_on_change, boolean()}
+          | {:after_only_on_failure, boolean()}
+  @type print_opts :: [print_opt()]
+  @spec enable_ir_printing(MLIR.PassManager.t(), print_opts()) :: :ok
+  def enable_ir_printing(%MLIR.PassManager{} = pm, opts \\ []) do
     MLIR.CAPI.mlirPassManagerEnableIRPrinting(
       pm,
-      Keyword.get(opts, :before_all, false),
-      Keyword.get(opts, :after_all, true),
-      Keyword.get(opts, :module_scope, false),
-      Keyword.get(opts, :after_only_on_change, false),
-      Keyword.get(opts, :after_only_on_failure, false)
+      !!Keyword.get(opts, :before_all, false),
+      !!Keyword.get(opts, :after_all, true),
+      !!Keyword.get(opts, :module_scope, false),
+      !!Keyword.get(opts, :after_only_on_change, false),
+      !!Keyword.get(opts, :after_only_on_failure, false)
     )
   end
 end
