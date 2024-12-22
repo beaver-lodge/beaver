402cefee2ff58d7b5f102b61596c169807266a4b
Shenghang Tsai <jackalcooper@gmail.com>
2024-12-22 11:58:31 +0800

    Run diagnostic in dirty scheduler

--- test/diagnostic_test.exs
+++ test/diagnostic_test.exs
@@ -1,7 +1,10 @@
 defmodule DiagnosticTest do
   use Beaver.Case, async: true, diagnostic: :server
-  alias Beaver.MLIR.Attribute
   alias Beaver.MLIR
+  use Beaver
+  alias MLIR.{Type, Attribute}
+  alias MLIR.Dialect.{Func, Builtin}
+  require Func
 
   defmodule DiagnosticTestHelper do
     def start_and_attach(ctx, cb) do
@@ -72,6 +75,42 @@ defmodule DiagnosticTest do
         )
 
       assert txt == @collected
+    end
+
+    defp unrealized_conversion_cast_f(ctx) do
+      import MLIR.Conversion
+
+      mlir ctx: ctx do
+        module do
+          Func.func some_func(
+                      function_type: Type.function([Type.i64()], [Type.i32()]),
+                      sym_name: MLIR.Attribute.string("f#{System.unique_integer([:positive])}")
+                    ) do
+            region do
+              block _(a >>> Type.i64()) do
+                v0 = Builtin.unrealized_conversion_cast(a) >>> Type.i32()
+                Func.return(v0) >>> []
+              end
+            end
+          end
+        end
+      end
+      |> convert_func_to_llvm()
+      |> Beaver.Composer.run!()
+      |> MLIR.ExecutionEngine.create!(dirty: :io_bound)
+    end
+
+    test "large invalid llvm ir", %{ctx: ctx} do
+      {_, d_str} =
+        MLIR.Context.with_diagnostics(
+          ctx,
+          fn ->
+            assert_raise RuntimeError, fn -> unrealized_conversion_cast_f(ctx) end
+          end,
+          fn d, _acc -> MLIR.to_string(d) end
+        )
+
+      assert d_str =~ "LLVM Translation failed for operation: builtin.unrealized_conversion_cast"
     end
   end
 end
