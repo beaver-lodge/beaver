b1a46894000a9b88eb22b34c66eba46a8c25c480
Shenghang Tsai <jackalcooper@gmail.com>
2025-01-29 11:20:58 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Beaver.MixProject do
         make_force_build: System.get_env("BEAVER_BUILD_CMAKE") in ["1", "true"],
         make_precompiler_url:
           System.get_env("BEAVER_ARTEFACT_URL") ||
-            "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2024-12-27-1014/@{artefact_filename}",
+            "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2025-01-29-0309/@{artefact_filename}",
         make_precompiler_nif_versions: [
           versions: fn opts ->
             target = opts.target
