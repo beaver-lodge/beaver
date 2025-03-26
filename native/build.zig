a507434ee4518601f264675af3ff77d125ac471e
Shenghang Tsai <jackalcooper@gmail.com>
2025-03-26 20:30:22 +0800

    Set zig build mode following mix env (#404)

--- native/build.zig
+++ native/build.zig
@@ -5,7 +5,18 @@ const os = builtin.os.tag;
 pub fn build(b: *std.Build) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const lib = b.addSharedLibrary(.{ .name = "BeaverNIF", .root_source_file = b.path("src/main.zig"), .optimize = .Debug, .target = b.standardTargetOptions(.{}) });
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const mix_env = std.process.getEnvVarOwned(allocator, "MIX_ENV") catch "";
+    var optimize: std.builtin.OptimizeMode = .ReleaseSafe;
+    if (std.mem.eql(u8, mix_env, "prod")) {
+        optimize = .ReleaseFast;
+    } else if (std.mem.eql(u8, mix_env, "test")) {
+        optimize = .Debug;
+    }
+    const lib = b.addSharedLibrary(.{ .name = "BeaverNIF", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = b.standardTargetOptions(.{}) });
+    std.log.info("Building {s}, MIX_ENV={s}, {?}\n", .{ lib.name, mix_env, lib.root_module.optimize });
     const kinda = b.dependency("kinda", .{});
     lib.root_module.addImport("kinda", kinda.module("kinda"));
     lib.root_module.addImport("erl_nif", kinda.module("erl_nif"));
