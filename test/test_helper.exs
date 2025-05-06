21b2880109f092f4867e283a9c380b4cd1ffc59a
Shenghang Tsai <jackalcooper@gmail.com>
2025-05-06 10:52:03 +0800

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,8 @@
 ExUnit.configure(
   exclude: [
     stderr: true,
-    cuda: :os.type() == {:unix, :darwin} or System.get_env("CI") == "true",
-    cuda_runtime: :os.type() == {:unix, :darwin} or System.get_env("CI") == "true"
+    cuda: :os.type() == {:unix, :darwin} or System.get_env("CI") != "true",
+    cuda_runtime: :os.type() == {:unix, :darwin} or System.get_env("CI") != "true"
   ]
 )
 
