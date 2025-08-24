b4f98d7bafc7fe530d346e9ffcf126b7d28c0e39
Shenghang Tsai <jackalcooper@gmail.com>
2025-08-24 12:03:33 +0800

    Update gen_header.exs

--- native/tools/wrapper/gen_header.exs
+++ native/tools/wrapper/gen_header.exs
@@ -8,8 +8,8 @@ mlir_include_dir = Path.expand(mlir_include_dir)
 files =
   ~w{mlir-c/**/*.h}
   |> Enum.flat_map(&Path.wildcard(Path.join(mlir_include_dir, &1)))
-  |> Enum.reject(&String.contains?(&1, "Python"))
-  |> Enum.reject(&String.contains?(&1, "LLVMIR"))
+  |> Enum.reject(&String.contains?(&1, "mlir-c/Bindings/Python"))
+  |> Enum.reject(&String.contains?(&1, "mlir-c/Target/LLVMIR.h"))
   |> Enum.map(&Path.relative_to(&1, mlir_include_dir))
 
 if Enum.empty?(files) do
