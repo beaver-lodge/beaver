914985c74be735492ba94646bcd36df1f464120c
Shenghang Tsai <jackalcooper@gmail.com>
2025-08-25 17:18:27 +0800

    Stream

--- native/tools/wrapper/gen_header.exs
+++ native/tools/wrapper/gen_header.exs
@@ -8,9 +8,9 @@ output = Keyword.fetch!(opts, :output)
 
 files =
   ~w{mlir-c/**/*.h}
-  |> Enum.flat_map(&Path.wildcard(Path.join(mlir_include_dir, &1)))
-  |> Enum.reject(&String.contains?(&1, "mlir-c/Bindings/Python"))
-  |> Enum.reject(&String.contains?(&1, "mlir-c/Target/LLVMIR.h"))
+  |> Stream.flat_map(&Path.wildcard(Path.join(mlir_include_dir, &1)))
+  |> Stream.reject(&String.contains?(&1, "mlir-c/Bindings/Python"))
+  |> Stream.reject(&String.contains?(&1, "mlir-c/Target/LLVMIR.h"))
   |> Enum.map(&Path.relative_to(&1, mlir_include_dir))
 
 if Enum.empty?(files) do
