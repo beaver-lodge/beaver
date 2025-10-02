9d5d9a3e3b7ca0bc7e353f7459fae7f5fca85edd
Shenghang Tsai <jackalcooper@gmail.com>
2025-10-02 21:51:06 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beaver.MixProject do
   def project do
     [
       app: :beaver,
-      version: "0.4.7-dev",
+      version: "0.4.8-dev",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -19,7 +19,7 @@ defmodule Beaver.MixProject do
         make_force_build: System.get_env("BEAVER_MAKE_FORCE_BUILD") in ["1", "true"],
         make_precompiler_url:
           System.get_env("BEAVER_ARTEFACT_URL") ||
-            "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2025-09-05-1313/@{artefact_filename}",
+            "https://github.com/beaver-lodge/beaver-prebuilt/releases/download/2025-10-02-1245/@{artefact_filename}",
         make_precompiler_nif_versions: [
           versions: fn _ -> ["2.16", "2.17"] end
         ],
