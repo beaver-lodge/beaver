2864d6aba2251c21829477382fca771d056ed2bf
Shenghang Tsai <jackalcooper@gmail.com>
2026-08-13 07:07:53 +0800

    declare iodata flatten term ABI

--- lib/beaver/mlir/conversion/ex.ex
+++ lib/beaver/mlir/conversion/ex.ex
@@ -159,6 +159,7 @@ defmodule Beaver.MLIR.Conversion.Ex do
     |> Plan.add_conversion_pattern("ex.file_read_lines", &convert_term_read/3, version: "1.0")
     |> Plan.add_conversion_pattern("ex.binary", &convert_term_binary/3, version: "1.0")
     |> Plan.add_conversion_pattern("ex.binary_from_list", &convert_term_read/3, version: "1.0")
+    |> Plan.add_conversion_pattern("ex.iodata_to_binary", &convert_term_read/3, version: "1.0")
     |> Plan.add_conversion_pattern("ex.float_lit", &convert_term_read/3, version: "1.0")
     |> Plan.add_conversion_pattern("ex.string_to_float", &convert_term_read/3, version: "1.0")
     |> Plan.add_conversion_pattern("ex.is_integer", &convert_term_predicate/3, version: "1.0")
@@ -289,6 +290,7 @@ defmodule Beaver.MLIR.Conversion.Ex do
     file_read: "ex.term.file_read",
     file_read_lines: "ex.term.file_read_lines",
     binary_from_list: "ex.term.binary_from_list",
+    iodata_to_binary: "ex.term.iodata_to_binary",
     float_lit: "ex.term.float_lit",
     string_to_float: "ex.term.string_to_float",
     is_integer: "ex.term.is_integer",
@@ -1028,6 +1030,7 @@ defmodule Beaver.MLIR.Conversion.Ex do
   defp read_intrinsic("ex.process_result"), do: @term_intrinsics.process_result
   defp read_intrinsic("ex.binary_length"), do: @term_intrinsics.binary_length
   defp read_intrinsic("ex.binary_from_list"), do: @term_intrinsics.binary_from_list
+  defp read_intrinsic("ex.iodata_to_binary"), do: @term_intrinsics.iodata_to_binary
   defp read_intrinsic("ex.binary_get"), do: @term_intrinsics.binary_get
   defp read_intrinsic("ex.binary_slice"), do: @term_intrinsics.binary_slice
   defp read_intrinsic("ex.binary_utf8_get"), do: @term_intrinsics.binary_utf8_get
--- test/ex_conversion_test.exs
+++ test/ex_conversion_test.exs
@@ -567,6 +567,7 @@ defmodule ExConversionTest do
             %6 = "ex.map_put"(%5, %2, %3) : (!ex.dyn, !ex.dyn, !ex.dyn) -> !ex.dyn
             %7 = "ex.binary"(%2, %3) {operandSegmentSizes = array<i32: 2>} : (!ex.dyn, !ex.dyn) -> !ex.dyn
             %8 = "ex.binary_from_list"(%4) : (!ex.dyn) -> !ex.dyn
+            %12 = "ex.iodata_to_binary"(%4) : (!ex.dyn) -> !ex.dyn
             %9 = "ex.float_lit"(%0) : (i64) -> !ex.dyn
             %10 = "ex.string_to_float"(%7) : (!ex.dyn) -> !ex.dyn
             %11 = "ex.is_list"(%4) : (!ex.dyn) -> i64
@@ -584,6 +585,7 @@ defmodule ExConversionTest do
     assert rendered =~ "ex.term.map_from_list"
     assert rendered =~ "ex.term.map_put"
     assert rendered =~ "ex.term.binary_from_list"
+    assert rendered =~ "ex.term.iodata_to_binary"
     assert rendered =~ "ex.term.float_lit"
     assert rendered =~ "ex.term.string_to_float"
     assert rendered =~ "ex.term.is_list"
